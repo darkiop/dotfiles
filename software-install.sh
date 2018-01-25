c2ac37c97474084b0c5e9a491febbab6f08d93de
Thorsten Walk <darkiop@gmail.com>
2018-01-25 17:31:07 +0100

    add unzip

--- software-install.sh
+++ software-install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo apt update
-sudo apt install powerline dnsutils vim byobu ranger htop cifs-utils net-tools html2text fping curl speedtest-cli
+sudo apt install powerline dnsutils vim byobu ranger htop cifs-utils net-tools html2text fping curl speedtest-cli unzip
