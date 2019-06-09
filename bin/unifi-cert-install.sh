e2dc1397e0a66cdcbc36dcea430f12b8ec601154
darkiop <darkiop@gmail.com>
2019-06-09 08:43:51 +0200

    Create unifi-cert-install.sh

---
+++ bin/unifi-cert-install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# Installiert SSL Zertifikate für den unifi-controller (docker). Zuvor müssen diese über den DSM (Synology) exportiert werden.
+
+echo "Installiere Zertifikat für den unifi-controller ..."
+echo "Der Export des Zertifikates vom Synology DSM muss unter /unifi/data/cert-temp liegen (3 Dateien)"
+echo "Achtung: Ein Passwort muss vergeben werden!"
+
+cd /unifi/data/cert-temp
+
+openssl pkcs12 -export -in cert.pem -inkey privkey.pem -out unifi.p12 -name unifi -CAfile fullchain.pem -caname root
+keytool -importkeystore -deststorepass aircontrolenterprise -destkeypass aircontrolenterprise -destkeystore /var/lib/unifi/keystore -srckeystore unifi.p12 -srcstoretype PKCS12 -alias unifi
+
+#EOF
