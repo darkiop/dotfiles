7977a762bb441b270abb31ca3e2dafc0f8794f41
darkiop <darkiop@gmail.com>
2019-06-21 07:29:18 +0200

    Update run-container_debian.sh

--- bin/docker/run-container_debian.sh
+++ bin/docker/run-container_debian.sh
@@ -1,1 +1,4 @@
+#!/bin/bash
+# Docker Start Skript: debian mit connect zu Docker auf odin
+
 docker run -it -d --name debian --hostname debian -v /var/run/docker.sock:/var/run/docker.sock darkiop/debian:latest
