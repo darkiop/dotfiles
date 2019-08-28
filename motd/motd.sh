c3e4b7fc045735885e3feaa9f1d5e43bdb88a152
Thorsten Walk <darkiop@gmail.com>
2019-08-28 18:27:26 +0200

    hostname switch

--- motd/motd.sh
+++ motd/motd.sh
@@ -55,9 +55,13 @@ red_color="\e[38;5;196m"
 green_color="\e[38;5;42m"
 green_color_bold="\e[1;38;5;42m"
 yellow_color="\e[38;5;227m"
-close_color="$(tput sgr0)"
 
-if [ -f ~/dotfiles/motd/tasks-$(hostname) ]; then
+case $get_host_name in
+  (odin)  close_color=""
+  (*)     close_color="$(tput sgr0)"
+esac
+
+if [ -f ~/dotfiles/motd/tasks-$get_host_name ]; then
   tasks="$(cat ~/dotfiles/motd/tasks-$(hostname))"
 else
   tasks="$(cat ~/dotfiles/motd/tasks)"
