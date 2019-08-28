8b25ffd56410c63999ea1535f73f45119bee3ed7
darkiop <darkiop@gmail.com>
2019-08-28 15:26:13 +0200

    Update motd.sh

--- motd/motd.sh
+++ motd/motd.sh
@@ -34,6 +34,7 @@ get_host_name=$(hostname)
 get_ip_host=$(/sbin/ip -o -4 addr list | awk '{print $4}' | cut -d/ -f1 | tail -1)
 #get_ip_host=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
 
+# --> Synology DSM: cat /etc.defaults/VERSION | grep productversion | awk -F'=' '{print $2}' | sed 's/"//' | sed 's/"//'
 get_plat_data=$(cat /etc/os-release | grep PRETTY_NAME | awk -F"=" '{print $2}' | awk -F'"' '{ print $2 }')
 
 get_os_load_1=$(cat /proc/loadavg | awk '{ print $1 }')
