10f2897346929a11fadb7a471dfe0e26ef6f9892
Thorsten Walk <darkiop@gmail.com>
2019-10-04 10:45:13 +0200

    add toilet

--- software-install.sh
+++ software-install.sh
@@ -2,6 +2,6 @@
 
 sudo apt update
 sudo apt upgrade -y
-sudo apt install -y powerline dnsutils vim byobu ranger htop cifs-utils net-tools html2text fping curl speedtest-cli unzip nmap iperf3 lnav lsof
+sudo apt install -y powerline dnsutils vim byobu ranger htop cifs-utils net-tools html2text fping curl speedtest-cli unzip nmap iperf3 lnav lsof toilet
 
 #EOF
