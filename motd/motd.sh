8e60c92ddb9539b34728d2f9e1b808699df69fe2
darkiop <darkiop@gmail.com>
2019-10-04 12:50:54 +0200

    Update motd.sh

--- motd/motd.sh
+++ motd/motd.sh
@@ -27,6 +27,7 @@ get_host_name=$(hostname)
 # get os version & ip & cputemp
 case $get_host_name in
   (odin)            get_plat_data="Synology DSM "$(cat /etc.defaults/VERSION | grep productversion | awk -F'=' '{print $2}' | sed 's/"//' | sed 's/"//')
+                    get_cpu_temp=$(($(cat /sys/class/hwmon/hwmon0/temp1_input)/1000))"°C"
                     get_ip_host=$(/sbin/ip -o -4 addr list ovs_eth0 | awk '{print $4}' | cut -d/ -f1);;
   (iobroker-hwr)    get_plat_data=$(cat /etc/os-release | grep PRETTY_NAME | awk -F"=" '{print $2}' | awk -F'"' '{ print $2 }')
                     get_cpu_temp=$(sudo vcgencmd measure_temp | egrep -o '[0-9]*\.[0-9]*')"°C"
