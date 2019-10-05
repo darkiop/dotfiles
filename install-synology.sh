cb1294ab788f8095ade3b5f7f67f972fb506ca9a
Thorsten Walk <darkiop@gmail.com>
2019-10-05 09:42:41 +0200

    fix

--- install-synology.sh
+++ install-synology.sh
@@ -6,3 +6,6 @@ wget -q https://raw.githubusercontent.com/darkiop/dotfiles/master/motd/motd.sh -
 wget -q https://raw.githubusercontent.com/darkiop/dotfiles/master/motd/motd-odin.sh -O $home/motd/motd-odin.sh
 wget -q https://raw.githubusercontent.com/darkiop/dotfiles/master/shells/alias -O $home/shells/alias
 wget -q https://raw.githubusercontent.com/darkiop/dotfiles/master/shells/alias-docker -O $home/shells/alias-docker
+
+clear
+reload
