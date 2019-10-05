9a631a96dd2fb0d8c04ae91770e24ee8d6ee25a3
Thorsten Walk <darkiop@gmail.com>
2019-10-05 09:32:01 +0200

    syno wgets

--- install-synology.sh
+++ install-synology.sh
@@ -2,5 +2,7 @@
 
 home=/var/services/homes/darkiop/dotfiles
 
+wget -q https://raw.githubusercontent.com/darkiop/dotfiles/master/motd/motd.sh -O $home/motd/motd.sh
+wget -q https://raw.githubusercontent.com/darkiop/dotfiles/master/motd/motd-odin.sh -O $home/motd/motd-odin.sh
 wget -q https://raw.githubusercontent.com/darkiop/dotfiles/master/shells/alias -O $home/shells/alias
 wget -q https://raw.githubusercontent.com/darkiop/dotfiles/master/shells/alias-docker -O $home/shells/alias-docker
