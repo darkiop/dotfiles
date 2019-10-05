6e69630794e5a1464f73e17271f159add0636b2d
Thorsten Walk <darkiop@gmail.com>
2019-10-05 10:11:09 +0200

    fix

--- install-synology.sh
+++ install-synology.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-home=/var/services/homes/darkiop/dotfiles
+dotfiles=/var/services/homes/darkiop/dotfiles
 
-wget -q --no-cache https://raw.githubusercontent.com/darkiop/dotfiles/master/install-synology.sh -O install-synology.sh
-wget -q --no-cache https://raw.githubusercontent.com/darkiop/dotfiles/master/motd/motd.sh -O $home/motd/motd.sh
-wget -q --no-cache https://raw.githubusercontent.com/darkiop/dotfiles/master/motd/motd-odin.sh -O $home/motd/motd-odin.sh
-wget -q --no-cache https://raw.githubusercontent.com/darkiop/dotfiles/master/shells/alias -O $home/shells/alias
-wget -q --no-cache https://raw.githubusercontent.com/darkiop/dotfiles/master/shells/alias-docker -O $home/shells/alias-docker
+wget -q --no-cache https://raw.githubusercontent.com/darkiop/dotfiles/master/install-synology.sh -O $dotfiles/install-synology.sh
+wget -q --no-cache https://raw.githubusercontent.com/darkiop/dotfiles/master/motd/motd.sh -O $dotfiles/motd/motd.sh
+wget -q --no-cache https://raw.githubusercontent.com/darkiop/dotfiles/master/motd/motd-odin.sh -O $dotfiles/motd/motd-odin.sh
+wget -q --no-cache https://raw.githubusercontent.com/darkiop/dotfiles/master/shells/alias -O $dotfiles/shells/alias
+wget -q --no-cache https://raw.githubusercontent.com/darkiop/dotfiles/master/shells/alias-docker -O $dotfiles/shells/alias-docker
+
+# EOF
