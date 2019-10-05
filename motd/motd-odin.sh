da54fdef30c2ddeb95cf8bc9dc887bc0e01bc632
Thorsten Walk <darkiop@gmail.com>
2019-10-05 09:51:42 +0200

    fix

--- motd/motd-odin.sh
+++ motd/motd-odin.sh
@@ -7,6 +7,7 @@ green_color="\e[38;5;42m"
 green_color_bold="\e[1;38;5;42m"
 yellow_color="\e[38;5;227m"
 
+echo
 echo -e $light_blue_color"running docker containers:"$green_color
 echo
 docker ps --format '{{.Names}}' | sort
