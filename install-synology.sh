4bcb8c9e04a74115031a993f75c15b184930b96c
Thorsten Walk <darkiop@gmail.com>
2019-10-05 09:19:28 +0200

    install syno

--- install-synology.sh
+++ install-synology.sh
@@ -2,4 +2,5 @@
 
 home=/var/services/homes/darkiop/dotfiles
 
+wget -q https://raw.githubusercontent.com/darkiop/dotfiles/master/shells/alias -O $home/shells/alias
 wget -q https://raw.githubusercontent.com/darkiop/dotfiles/master/shells/alias-docker -O $home/shells/alias-docker
