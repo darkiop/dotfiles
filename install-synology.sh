1d9c60e2f2e4b56ed27acfc56f429e5fdbaf4c7a
Thorsten Walk <darkiop@gmail.com>
2019-10-05 09:43:37 +0200

    fix

--- install-synology.sh
+++ install-synology.sh
@@ -2,6 +2,7 @@
 
 home=/var/services/homes/darkiop/dotfiles
 
+wget -q https://raw.githubusercontent.com/darkiop/dotfiles/master/install-synology.sh -O install-synology.sh
 wget -q https://raw.githubusercontent.com/darkiop/dotfiles/master/motd/motd.sh -O $home/motd/motd.sh
 wget -q https://raw.githubusercontent.com/darkiop/dotfiles/master/motd/motd-odin.sh -O $home/motd/motd-odin.sh
 wget -q https://raw.githubusercontent.com/darkiop/dotfiles/master/shells/alias -O $home/shells/alias
