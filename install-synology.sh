5483734385277fac5e1b3830410b3e8dce0452d9
Thorsten Walk <darkiop@gmail.com>
2019-10-05 09:48:58 +0200

    fix

--- install-synology.sh
+++ install-synology.sh
@@ -2,8 +2,8 @@
 
 home=/var/services/homes/darkiop/dotfiles
 
-wget -q https://raw.githubusercontent.com/darkiop/dotfiles/master/install-synology.sh -O install-synology.sh
-wget -q https://raw.githubusercontent.com/darkiop/dotfiles/master/motd/motd.sh -O $home/motd/motd.sh
-wget -q https://raw.githubusercontent.com/darkiop/dotfiles/master/motd/motd-odin.sh -O $home/motd/motd-odin.sh
-wget -q https://raw.githubusercontent.com/darkiop/dotfiles/master/shells/alias -O $home/shells/alias
-wget -q https://raw.githubusercontent.com/darkiop/dotfiles/master/shells/alias-docker -O $home/shells/alias-docker
+wget -q --no-cache https://raw.githubusercontent.com/darkiop/dotfiles/master/install-synology.sh -O install-synology.sh
+wget -q --no-cache https://raw.githubusercontent.com/darkiop/dotfiles/master/motd/motd.sh -O $home/motd/motd.sh
+wget -q --no-cache https://raw.githubusercontent.com/darkiop/dotfiles/master/motd/motd-odin.sh -O $home/motd/motd-odin.sh
+wget -q --no-cache https://raw.githubusercontent.com/darkiop/dotfiles/master/shells/alias -O $home/shells/alias
+wget -q --no-cache https://raw.githubusercontent.com/darkiop/dotfiles/master/shells/alias-docker -O $home/shells/alias-docker
