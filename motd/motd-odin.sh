3b5f4d4a91a03860b8cecfeb04dcfcce95b69937
Thorsten Walk <darkiop@gmail.com>
2019-10-06 12:52:28 +0200

    docker ps

--- motd/motd-odin.sh
+++ motd/motd-odin.sh
@@ -10,7 +10,7 @@ yellow_color="\e[38;5;227m"
 echo
 echo -e $light_blue_color"running docker containers:"$green_color
 echo
-docker ps --format '{{.Names}}' | sort
+docker ps --format 'table {{.Names}}\t{{.Status}}\t{{.Networks}}' | sed -n '1!p' | sort
 echo
 
 #EOF
