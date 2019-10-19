796d7d04500187d53df26fd9a91499a0fef15266
Thorsten Walk <darkiop@gmail.com>
2019-10-19 05:55:52 +0200

    iprout2 procps

--- install-applications.sh
+++ install-applications.sh
@@ -2,6 +2,6 @@
 
 sudo apt update
 sudo apt upgrade -y
-sudo apt install -y powerline dnsutils vim byobu ranger htop cifs-utils net-tools html2text fping curl speedtest-cli unzip nmap iperf3 lnav lsof toilet command-not-found bash-completion git
+sudo apt install -y powerline dnsutils vim byobu ranger htop cifs-utils net-tools html2text fping curl speedtest-cli unzip nmap iperf3 lnav lsof toilet command-not-found bash-completion git iproute2 procps
 
 #EOF
