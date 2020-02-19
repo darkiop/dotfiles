ea9bb147ae297760d58e3d290ff443b4c7a3c244
Thorsten Walk <darkiop@gmail.com>
2020-02-19 15:59:47 +0100

    chmod

--- install-applications.sh
+++ install-applications.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install -y powerline dnsutils vim byobu ranger htop cifs-utils net-tools html2text fping curl speedtest-cli unzip nmap iperf3 lnav lsof toilet command-not-found bash-completion git iproute2 procps
+sudo apt install -y powerline dnsutils vim byobu ranger htop cifs-utils net-tools html2text fping curl speedtest-cli unzip nmap iperf3 lnav lsof toilet command-not-found bash-completion git iproute2 procps tcpdump
 
 # EOF
