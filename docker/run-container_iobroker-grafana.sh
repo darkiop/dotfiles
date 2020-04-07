807673f6e8284e928fb3e9d10f039077c9f087a4
darkiop <darkiop@gmail.com>
2020-04-07 21:33:55 +0200

    Delete run-container_iobroker-grafana.sh

--- docker/run-container_iobroker-grafana.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Docker Start Skript: iobroker-grafana
-
-chown 104:104 /volume1/docker/iobroker-grafana
-
-docker run -it -d --name=iobroker-grafana --hostname iobroker-grafana -p 3000:3000 --restart no --user 104 -v /volume1/docker/iobroker-grafana:/var/lib/grafana grafana/grafana
