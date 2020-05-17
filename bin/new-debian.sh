8cf5a5709be31f85c2d764f3521abecd437f8d47
darkiop <darkiop@gmail.com>
2020-05-17 07:17:07 +0200

    Create new-debian.sh

---
+++ bin/new-debian.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+apt update
+
+apt upgrade -y
+
+apt install sudo git
+
+adduser darkiop
+
+usermod -a -G sudo darkiop
+
+ln -fs /usr/share/zoneinfo/Europe/Berlin /etc/localtime
+
+dpkg-reconfigure -f noninteractive tzdata
+
+dpkg-reconfigure locales
+
+su darkiop
+
+mkdir ~/dotfiles && cd ~/dotfiles && git clone https://github.com/darkiop/dotfiles.git . && ./install-applications.sh && ./install-bashrc.sh && source ~/.bashrc
