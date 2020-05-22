0c7f103969f3921598c476ab0437edfa30c9cd7f
darkiop <darkiop@gmail.com>
2020-05-22 07:27:54 +0200

    Create docker-add-route-mac0.sh

---
+++ bin/docker-add-route-mac0.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+if ip link | grep "mac0@ovs_eth0" > /dev/null; then
+  echo "Device mac0 existiert"
+else
+  echo "Device mac0 anlegen"
+  ip link del mac0
+  ip link add mac0 link ovs_eth0 type macvlan mode bridge
+  ip addr add 192.168.1.80/32 dev mac0
+  ip link set mac0 up
+  ip route add 192.168.1.80/28 dev mac0
+fi
