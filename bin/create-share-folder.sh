3480fb9ae84c8dd74e97dd71fb9398031ccf5584
darkiop <darkiop@gmail.com>
2020-05-22 13:05:03 +0200

    Create create-share-folder.sh

---
+++ bin/create-share-folder.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+#
+
+# mkdir if not exist (todo)
+mkdir ~/share
+
+mkdir ~/share/odin
+mkdir ~/share/odin/home
+mkdir ~/share/odin/backup
+mkdir ~/share/odin/media
+mkdir ~/share/odin/proxmox
+
+mkdir ~/share/thor
+mkdir ~/share/thor/downloads
+
+mkdir ~/share/iobroker-hwr
+mkdir ~/share/iobroker-hwr/home
+
+mkdir ~/share/pve-ct-pihole
+mkdir ~/share/pve-ct-pihole/home
+
+mkdir ~/share/pve-ct-unifi
+mkdir ~/share/pve-ct-unifi/home
+
+# mounts (todo)
+# ...
+
+# EOF
