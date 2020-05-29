6b8e2677e9a4827503baaebe403bb58b5cce91bf
darkiop <darkiop@gmail.com>
2020-05-29 11:22:20 +0200

    Update install-applications.sh

--- install-applications.sh
+++ install-applications.sh
@@ -8,8 +8,17 @@ green_color_bold="\e[1;38;5;42m"
 yellow_color="\e[38;5;227m"
 close_color="$(tput sgr0)"
 
+# install software
 sudo apt update
-sudo apt install -y powerline dnsutils vim byobu ranger htop cifs-utils net-tools html2text fping curl speedtest-cli unzip nmap iperf3 lnav lsof toilet command-not-found bash-completion git iproute2 procps tcpdump man wakeonlan neofetch mlocate telnet rcconf nfs-common
+sudo apt install -y \
+powerline \
+dnsutils \
+vim \
+byobu \
+ranger \
+htop cifs-utils net-tools html2text fping curl speedtest-cli unzip nmap iperf3 lnav lsof toilet command-not-found bash-completion git iproute2 procps tcpdump man wakeonlan neofetch mlocate telnet nfs-common
+
+# tmp: rcconf
 
 # install lsd
 echo
