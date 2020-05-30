0036c09f01b13b33554f8f9429de0818ebcb5227
darkiop <darkiop@gmail.com>
2020-05-30 09:01:29 +0200

    Update iobroker-backup-sync-iobroker-mariadb.sh

--- bin/iobroker/iobroker-backup-sync-iobroker-mariadb.sh
+++ bin/iobroker/iobroker-backup-sync-iobroker-mariadb.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #
-# Kopiert CCU Backups nach NFS Mount /mnt/odin/backup/ccu
+# Kopiert iobroker-mariadb Backups nach NFS Mount /mnt/odin/backup/iobroker-mariadb
 # fstab: 192.168.1.43:/volume1/backup /mnt/odin/backup nfs rw 0 0
-# crontab: 30 4 * * * /home/darkiop/dotfiles/bin/iobroker/iobroker-backup-sync-ccu.sh
+# crontab: 30 4 * * * /home/darkiop/dotfiles/bin/iobroker/iobroker-backup-sync-iobroker-mariadb.sh
 
-sudo rsync -avz --exclude=mysql_* --exclude=iobroker_* --delete /home/darkiop/docker/prod/iobroker-master/opt-iobroker/backups/ /mnt/odin/backup/ccu
+sudo rsync -avz --exclude=homematic_* --exclude=iobroker_* --delete /home/darkiop/docker/prod/iobroker-master/opt-iobroker/backups/ /mnt/odin/backup/iobroker-mariadb
 
 # EOF
