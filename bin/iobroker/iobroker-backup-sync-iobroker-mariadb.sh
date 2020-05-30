fb23b69586de260b9e8d36855630d042050bb30d
darkiop <darkiop@gmail.com>
2020-05-30 09:20:55 +0200

    Update iobroker-backup-sync-iobroker-mariadb.sh

--- bin/iobroker/iobroker-backup-sync-iobroker-mariadb.sh
+++ bin/iobroker/iobroker-backup-sync-iobroker-mariadb.sh
@@ -4,6 +4,23 @@
 # fstab: 192.168.1.43:/volume1/backup /mnt/odin/backup nfs rw 0 0
 # crontab: 30 4 * * * /home/darkiop/dotfiles/bin/iobroker/iobroker-backup-sync-iobroker-mariadb.sh
 
-sudo rsync -avz --exclude=homematic_* --exclude=iobroker_* --delete /home/darkiop/docker/prod/iobroker-master/opt-iobroker/backups/ /mnt/odin/backup/iobroker-mariadb
+# check mountpoint
+if mountpoint -q /mnt/odin/backup
+then
+  # run rsync
+  sudo rsync -avz --exclude=homematic_* --exclude=iobroker_* --delete /home/darkiop/docker/prod/iobroker-master/opt-iobroker/backups/ /mnt/odin/backup/iobroker-mariadb
+else
+  # mount
+  mount /mnt/odin/backup
+  # check again
+  if mountpoint -q /mnt/odin/backup
+  then
+    # run rsync
+    sudo rsync -avz --exclude=homematic_* --exclude=iobroker_* --delete /home/darkiop/docker/prod/iobroker-master/opt-iobroker/backups/ /mnt/odin/backup/iobroker-mariadb
+  else
+    # exit
+    exit 0
+  fi
+fi
 
 # EOF
