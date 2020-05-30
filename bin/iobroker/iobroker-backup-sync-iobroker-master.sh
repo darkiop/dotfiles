1d242d90783bf3c3c76f1e9bce1065e9db854bed
darkiop <darkiop@gmail.com>
2020-05-30 08:52:30 +0200

    Update and rename iobroker-backup-sync-iobroker.sh to iobroker-backup-sync-iobroker-master.sh

---
+++ bin/iobroker/iobroker-backup-sync-iobroker-master.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# Kopiert CCU Backups nach NFS Mount /mnt/odin/backup/iobroker-master
+# fstab: 192.168.1.43:/volume1/backup /mnt/odin/backup nfs rw 0 0
+# crontab: 30 4 * * * /home/darkiop/dotfiles/bin/iobroker/iobroker-backup-sync-iobroker-master.sh
+
+sudo rsync -avz --exclude=mysql_* --exclude=homematic_* --delete /home/darkiop/docker/prod/iobroker-master/opt-iobroker/backups/ /mnt/odin/backup/iobroker-master
+
+# EOF
--- bin/iobroker/iobroker-backup-sync-iobroker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# Kopiert CCU Backups nach NFS Mount /mnt/odin/backup/ccu
-# fstab: 192.168.1.43:/volume1/backup /mnt/odin/backup nfs rw 0 0
-# crontab: 30 4 * * * /home/darkiop/dotfiles/bin/iobroker/iobroker-backup-sync-ccu.sh
-
-sudo rsync -avz --exclude=mysql_* --exclude=iobroker_* --delete /home/darkiop/docker/prod/iobroker-master/opt-iobroker/backups/ /mnt/odin/backup/ccu
-
-# EOF
