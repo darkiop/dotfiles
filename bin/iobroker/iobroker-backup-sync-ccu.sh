224355b756e3830cf9f44ad4b7fad65296ab0c6c
darkiop <darkiop@gmail.com>
2020-05-30 08:20:59 +0200

    Update iobroker-backup-sync-ccu.sh

--- bin/iobroker/iobroker-backup-sync-ccu.sh
+++ bin/iobroker/iobroker-backup-sync-ccu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-rsync -avz --exclude=mysql_* --exclude=iobroker_* /home/darkiop/docker/prod/iobroker-master/opt-iobroker/backups/ /mnt/odin/backup/ccu
+sudo rsync -avz --exclude=mysql_* --exclude=iobroker_* --delete /home/darkiop/docker/prod/iobroker-master/opt-iobroker/backups/ /mnt/odin/backup/ccu
 
 # EOF
