fe026dadfbaf8b18fc718e050a004f212dd0ad47
darkiop <darkiop@gmail.com>
2020-05-30 08:25:53 +0200

    Update iobroker-backup-sync-ccu.sh

--- bin/iobroker/iobroker-backup-sync-ccu.sh
+++ bin/iobroker/iobroker-backup-sync-ccu.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#
+# Kopiert CCU Backups nach NFS Mount /mnt/odin/backup/ccu
+# fstab: 192.168.1.43:/volume1/backup /mnt/odin/backup nfs rw 0 0
 
 sudo rsync -avz --exclude=mysql_* --exclude=iobroker_* --delete /home/darkiop/docker/prod/iobroker-master/opt-iobroker/backups/ /mnt/odin/backup/ccu
 
