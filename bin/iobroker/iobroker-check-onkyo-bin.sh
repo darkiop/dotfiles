53b29196ea44012726b6849f1d04fb9a3e0aa3ab
darkiop <darkiop@gmail.com>
2020-06-04 18:54:48 +0200

    Update iobroker-check-onkyo-bin.sh

--- bin/iobroker/iobroker-check-onkyo-bin.sh
+++ bin/iobroker/iobroker-check-onkyo-bin.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+# custom script for iobroker Container
+# check if onkyo-eiscp is installed
+# pip3 should be installed by docker run
 
-if [ -f /usr/local/bin/onkyo ]; then
-
+if [ -f /usr/bin/pip3 ]; then
+  if [ ! -f /usr/local/bin/onkyo ]; then
+    pip3 install onkyo-eiscp
+  fi
 fi
 
 #EOF
