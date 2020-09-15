aa5b4ad55f4fa76feb20ae4dfd720bcc2398a1a9
darkiop <darkiop@gmail.com>
2020-09-15 17:39:34 +0200

    Update dsm-backup.sh

--- bin/dsm-backup.sh
+++ bin/dsm-backup.sh
@@ -2,7 +2,7 @@
 # create DSM backup
 # https://www.bachmann-lan.de/synology-dsm-6-konfiguration-automatisch-per-cli-sichern/
 
-/usr/syno/bin/synoconfbkp export --filepath=/volume1/backup/dsm/`hostname`_`date +%Y.%m.%d`.dss
+/usr/syno/bin/synoconfbkp export --filepath=/volume1/backup/dsm/`hostname`_`date +%Y%m%d_%H%M%S`.dss
 
 # delete backups older than 14 days
 find /volume1/backup/dsm/ -name "*.dss" -mtime +14 -exec rm {} \;
