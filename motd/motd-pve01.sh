0396065b6d1cbcf2819c92654315d46882118b6a
Thorsten Walk <darkiop@gmail.com>
2020-12-21 08:26:33 +0100

    motd pve01 + alias

---
+++ motd/motd-pve01.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# load colors
+blue_color="\e[38;5;33m"
+light_blue_color="\e[38;5;39m"
+red_color="\e[38;5;196m"
+green_color="\e[38;5;42m"
+green_color_bold="\e[1;38;5;42m"
+yellow_color="\e[38;5;227m"
+close_color="$(tput sgr0)"
+
+echo
+
+echo -e "$blue_color"List Proxmox Container"$close_color"
+echo -e "$green_color"
+sudo pct list
+echo -e "$green_color"
+
+echo -e "$blue_color"List Proxmox Virtual Machines"$close_color"
+echo -e "$green_color"
+# | sed -e 's/^[ \t]*//' = remove leading zeros
+sudo qm list | sed -e 's/^[ \t]*//'
+echo -e "$green_color"
+
+#EOF
