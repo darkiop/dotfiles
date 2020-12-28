77830fea367c80284cf15cd3b320be9a6e622df3
Thorsten Walk <darkiop@gmail.com>
2020-12-28 15:59:43 +0100

    install from git

--- install-from-git.sh
+++ install-from-git.sh
@@ -3,12 +3,6 @@
 # install dotfies from github
 # curl -sL https://raw.githubusercontent.com/darkiop/dotfiles/master/install-from-git.sh | bash -
 
-# check for curl + git and install if necessary
-if [!dpkg -s git >/dev/null 2>&1]; then
-  sudo apt update
-  sudo apt install git -y
-fi
-
 # git clone
 git clone https://github.com/darkiop/dotfiles $HOME/dotfiles
 
