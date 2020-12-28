56896a1b5a943af21382c0b312b310dbd3701d64
Thorsten Walk <darkiop@gmail.com>
2020-12-28 16:03:45 +0100

    install from git

--- install-from-git.sh
+++ install-from-git.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-#
 # install dotfies from github
 # curl -sL https://raw.githubusercontent.com/darkiop/dotfiles/master/install-from-git.sh | bash -
 
 # git clone
+echo
+echo -e $green_color"clone"$close_color$yellow_color" dotfiles "$green_color"from github ..."$close_color
 git clone https://github.com/darkiop/dotfiles $HOME/dotfiles
 
 # install
-#bash $HOME/dotfiles/install-applications.sh
-#bash $HOME/dotfiles/install-bashrc.sh
+bash $HOME/dotfiles/install-applications.sh
+bash $HOME/dotfiles/install-bashrc.sh
 
 # EOF
