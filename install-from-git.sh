404c1c235e6ece54e041240d8e59e462f359405f
Thorsten Walk <darkiop@gmail.com>
2020-12-28 16:19:24 +0100

    install from git

--- install-from-git.sh
+++ install-from-git.sh
@@ -3,23 +3,26 @@
 # curl -sL https://raw.githubusercontent.com/darkiop/dotfiles/master/install-from-git.sh | bash -
 
 if [ -d $HOME/dotfiles ]; then
+  # dotfiles dir exist
   echo -e "$red_color"
   read -p "dotfiles exists - Re-Install! Are you sure? ~/dotfiles will be deleted! (y/n):" reinstall
   echo -e "$close_color"
   if [ $reinstall == "y" ]; then
     curl -sL https://raw.githubusercontent.com/darkiop/dotfiles/master/install-reinstall.sh | bash -
   else
+    echo -e $red_color"exit ..."$close_color
     exit
   fi
 else
-  # git clone
-  echo
+  # dotfiles dir do not exist
+  
+  # clone from git
   echo -e $green_color"clone"$close_color$yellow_color" dotfiles "$green_color"from github ..."$close_color
   git clone https://github.com/darkiop/dotfiles $HOME/dotfiles
+  
+  # install
+  bash $HOME/dotfiles/install-applications.sh
+  bash $HOME/dotfiles/install-bashrc.sh
 fi
-
-# install
-bash $HOME/dotfiles/install-applications.sh
-bash $HOME/dotfiles/install-bashrc.sh
 
 # EOF
