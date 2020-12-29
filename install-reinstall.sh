938b29464d59ea57fddae0f8d98c98b19e587331
Thorsten Walk <darkiop@gmail.com>
2020-12-29 10:34:53 +0100

    reinstall color

--- install-reinstall.sh
+++ install-reinstall.sh
@@ -6,7 +6,7 @@ echo -e "$close_color"
 
 if [ $reinstall == "y" ]; then
   cd $HOME
-  echo -e $green_color"delete"$close_color$yellow_color" ~/dotfiles "$green_color"..."$close_color
+  echo -e $red_color"delete"$close_color$yellow_color" ~/dotfiles "$green_color"..."$close_color
   sudo rm -r $HOME/dotfiles
   echo
   echo -e $green_color"reinstall "$close_color$yellow_color" ~/dotfiles "$green_color"..."$close_color
