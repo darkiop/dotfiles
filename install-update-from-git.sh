e502672ff717301033e9bbd30d14a5435046a73b
Thorsten Walk <darkiop@gmail.com>
2020-12-29 10:11:32 +0100

    update script

--- install-update-from-git.sh
+++ install-update-from-git.sh
@@ -4,6 +4,9 @@ git status > /dev/null 2>&1 &
 
 if git diff-index --quiet HEAD --; then
   # no changes
+  echo
+  echo -e $red_color"No changes to the dotfiles were found. Update ..."$close_color
+  echo
   cd ~/dotfiles
   git pull
   cd ~
@@ -11,7 +14,7 @@ if git diff-index --quiet HEAD --; then
 else
   # changes
   echo
-  echo -e $red_color"Local changes to the dotfiles were found. Check and commit these or run install-reinstall.sh. "$close_color
+  echo -e $red_color"Local changes to the dotfiles were found. Check and commit these or run install-reinstall.sh."$close_color
   echo
 fi
 
