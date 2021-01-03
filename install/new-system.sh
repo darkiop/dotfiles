b6a6b8b4402d2827a6ac7a4e23247264b491a223
Thorsten Walk <darkiop@gmail.com>
2021-01-03 16:18:07 +0100

    setup

--- install/new-system.sh
+++ install/new-system.sh
@@ -3,17 +3,98 @@
 # install curl and execute
 # -> curl -sL https://raw.githubusercontent.com/darkiop/dotfiles/master/bin/new-debian.sh | bash -
 
+# set colors
+blue_color="\e[38;5;33m"
+light_blue_color="\e[38;5;39m"
+red_color="\e[38;5;196m"
+green_color="\e[38;5;42m"
+green_color_bold="\e[1;38;5;42m"
+yellow_color="\e[38;5;227m"
+close_color="$(tput sgr0)"
+
+# ask functions
+ask() {
+  local color="$1"
+  case $color in
+    green)
+    color=$green_color
+    ;;
+    blue)
+    color=$blue_color
+    ;;
+    lightblue)
+    color=$light_blue_color
+    ;;
+    yellow)
+    color=$yellow_color
+    ;;
+    red)
+    color=$yellow_color
+    ;;
+  esac
+  while true; do
+    echo -e "$color"
+    read -p "$2 ([y]/n) " -r
+    echo -e "$close_color"
+    REPLY=${REPLY:-"y"}
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+      return 1
+    elif [[ $REPLY =~ ^[Nn]$ ]]; then
+      return 0
+    fi
+  done
+}
+
+# info function
+infomsg() {
+  local color="$1"
+  case $color in
+    green)
+    color=$green_color
+    ;;
+    blue)
+    color=$blue_color
+    ;;
+    lightblue)
+    color=$light_blue_color
+    ;;
+    yellow)
+    color=$yellow_color
+    ;;
+    red)
+    color=$yellow_color
+    ;;
+  esac
+  echo -e "$color"
+  echo "$1"
+  echo -e "$close_color"
+}
+
 # system updates
+infomsg green_color "update the system ..."
 apt update
 apt upgrade -y
 
 # install sudo & git
-apt install -Y sudo git curl wget
+infomsg green_color "install git, curl and wget ..."
+apt install -y sudo git curl wget
 
 # time & locales
 ln -fs /usr/share/zoneinfo/Europe/Berlin /etc/localtime
+apt-get install -y tzdata
 dpkg-reconfigure -f noninteractive tzdata
-dpkg-reconfigure locales
+
+#pkg-reconfigure locales
+
+echo "Europe/Berlin" > /etc/timezone
+dpkg-reconfigure -f noninteractive tzdata
+sed -i -e 's/# de_DE.UTF-8 UTF-8/de_DE.UTF-8 UTF-8/' /etc/locale.gen
+echo 'LANG="de_DE.UTF-8"'>/etc/default/locale
+dpkg-reconfigure --frontend=noninteractive locales
+update-locale LANG=de_DE.UTF-8
+
+
+
 
 # add user darkiop
 useradd -m -s /bin/bash darkiop
