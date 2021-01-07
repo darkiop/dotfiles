6e941f140376dd07dee8242fc249ebc3fe8fd0f8
Thorsten Walk <darkiop@gmail.com>
2021-01-07 12:28:54 +0100

    motd

--- motd/motd-proxmox.sh
+++ motd/motd-proxmox.sh
@@ -2,16 +2,14 @@
 
 echo -e "$blue_color"Proxmox Version"$close_color   `echo -e "$green_color$(pveversion)$close_color"`"
 echo
-
 echo -e "$blue_color"List Proxmox Container"$close_color"
-echo -e "$green_color"
-sudo pct list
-echo -e "$green_color"
-
+echo
+echo -e $yellow_color"`sudo pct list` "$close_color
+echo
 echo -e "$blue_color"List Proxmox Virtual Machines"$close_color"
-echo -e "$green_color"
+echo
 # | sed -e 's/^[ \t]*//' = remove leading zeros
-sudo qm list | sed -e 's/^[ \t]*//'
-echo -e "$green_color"
+#sudo qm list | sed -e 's/^[ \t]*//'
+echo -e $yellow_color"`sudo qm list | sed -e 's/^[ \t]*//'` "$close_color
 
 #EOF
