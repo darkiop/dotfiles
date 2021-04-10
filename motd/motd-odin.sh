81f2293a353d5f07a4d39d2604184d1d96b030ba
Thorsten Walk <darkiop@gmail.com>
2021-04-10 09:13:57 +0200

    motd

--- motd/motd-odin.sh
+++ motd/motd-odin.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# size of /volume1
+volume1_usage=$(df -h /volume1 | awk '/\// {print $(NF-1)}' | sed 's/%//g')
+volume1_usage_gb=$(df -h /volume1 | awk '/\// {print $(NF-3)}')
+volume1_total=$(df -h /volume1 | awk '/\// {print $(NF-4)}')
+
+echo -e "$blue_color"usage of /volume1"$close_color        `echo -e "$green_color$volume1_usage_gb$close_color"` "of" `echo -e "$green_color$volume1_total$close_color"` ($volume1_usage%)"
+
 echo
 
 #EOF
