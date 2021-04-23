58696ab85822dc95c3c9be1afab94ae58d96b185
Thorsten Walk <darkiop@gmail.com>
2021-04-23 09:34:04 +0200

    checkmk local-check iobroker work-in-progress

--- checkmk/local-checks/iobroker.sh
+++ checkmk/local-checks/iobroker.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 # CheckMK-Documentation: https://docs.checkmk.com/latest/de/localchecks.html
-# /usr/lib/check_mk_agent/local
+#
+# Install: /usr/lib/check_mk_agent/local
 #
 #                  |----| |---------| |--------------| |-----------------------------------------|
 # example output: "STATUS SERVICENAME METRICS=73;80;90 STATUS DETAIL TEXT WHICH CAN CONTAIN SPACES"
 #
 # Status: 0 = OK | 1 = WARN | 2 = CRIT | 3 = UNKNOWN | P = dynamic
+#
+# iobProcess=$(/usr/bin/ps -o cmd -C "node" --no-headers | grep ^iobroker.js-controller 1>&2 | echo $?)
 
-# ioBroker Processes
-#iobProcess=$(/usr/bin/ps -o cmd -C "node" --no-headers | grep ^iobroker.js-controller 1>&2 | echo $?)
+#
+# ioBroker Process: iobroker.js-controller
+#
 iobProcess=$(pidof iobroker.js-controller)
 if [ "$?" -ne 0 ]; then
   status=2
@@ -17,11 +21,28 @@ else
   status=0
   statusdetail='running'
 fi
-servicename='ioBroker.js-controller'
+servicename='ioBroker.iobroker.js-controller'
 metrics='-'
 echo "$status $servicename $metrics $statusdetail"
 
+#
+# ioBroker Process: io.javascript.0
+#
+iobProcess=$(pidof io.javascript.0)
+if [ "$?" -ne 0 ]; then
+  status=2
+  statusdetail='not running'
+else
+  status=0
+  statusdetail='running'
+fi
+servicename='ioBroker.io.javascript.0'
+metrics='-'
+echo "$status $servicename $metrics $statusdetail"
+
+#
 # ioBroker Status
+#
 iobStatus=$(iobroker status | head -n1)
 if [[ $iobStatus == *"not"* ]]; then
   status=2
@@ -33,8 +54,10 @@ metrics='-'
 statusdetail=$iobStatus
 echo "$status $servicename $metrics $statusdetail"
 
+#
 # number of processes
 # TODO metrics
+#
 iobNumberOfProcesses=$(/usr/bin/ps -o cmd -C "node" --no-headers | wc -l)
 status=0
 servicename='ioBroker.NumberOfProcesses'
@@ -42,9 +65,11 @@ metrics='-'
 statusdetail=$iobNumberOfProcesses
 echo "$status $servicename $metrics $statusdetail"
 
+#
 # ioBroker getvalue
 # TODO metrics
 # TODO error handling (iobroker is not runnig)
+#
 iobGetvalue=$(iobroker state getvalue smartmeter.1.1-0:16_7_0__255.value)
 if [[ $iobGetvalue -gt 400 && $iobGetvalue -lt 1000 ]]; then
   status=1
