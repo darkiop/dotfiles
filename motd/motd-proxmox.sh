a6ac70a9658bfd0d3b3f483a7c42480c08f71808
Thorsten Walk <darkiop@gmail.com>
2021-05-13 08:46:12 +0200

    motd

--- motd/motd-proxmox.sh
+++ motd/motd-proxmox.sh
@@ -4,10 +4,10 @@ echo -e "$blue_color"Proxmox Version"$close_color   `echo -e "$green_color$(pvev
 echo
 echo -e "$light_blue_color"List Proxmox Container"$close_color"
 echo
-echo -e $yellow_color"`pct list | sed '1d'` | sed "s,.*,$(echo -e $yellow_color)&$close_color," | sed "s,running,$(echo -e $green_color)&$close_color," | sed "s,stopped,$(echo -e $red_color)&$close_color,""$close_color
+echo -e $yellow_color"`pct list | sed -e 's/^[ \t]*//' | sed '1d' | sed "s,.*,$(echo -e $yellow_color)&$close_color," | sed "s,running,$(echo -e $green_color)&$close_color," | sed "s,stopped,$(echo -e $red_color)&$close_color,"`"$close_color | awk '{print $1,$3,$2}' | sed -e's/  */ /g'
 echo
 echo -e "$light_blue_color"List Proxmox Virtual Machines"$close_color"
 echo
-echo -e $yellow_color"`qm list | sed -e 's/^[ \t]*//' | sed '1d' | awk '{print $1,$2,$3}' | column -t | sed "s,.*,$(echo -e $yellow_color)&$close_color," | sed "s,running,$(echo -e $green_color)&$close_color," | sed "s,stopped,$(echo -e $red_color)&$close_color,"`"$close_color
+echo -e $yellow_color"`qm list | sed -e 's/^[ \t]*//' | sed '1d' | sed "s,.*,$(echo -e $yellow_color)&$close_color," | sed "s,running,$(echo -e $green_color)&$close_color," | sed "s,stopped,$(echo -e $red_color)&$close_color,"`"$close_color | awk '{print $1,$2,$3}' | sed -e's/  */ /g'
 
 #EOF
