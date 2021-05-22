3a6efd1a53030e13eb1ad8ef210589239272d40d
Thorsten Walk <darkiop@gmail.com>
2021-05-22 08:03:57 +0200

    config fail2ban

---
+++ install/config-fail2ban.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ -f /etc/fail2ban/jail.conf ]; then
+  cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
+  sed -i 's/^bantime[[:blank:]]*= 10m/bantime = 24h/g' /etc/fail2ban/jail.local
+  sed -i 's/^findtime[[:blank:]]*= 10m/findtime = 1h/g' /etc/fail2ban/jail.local
+  sed -i 's/^maxretry[[:blank:]]*= 5/maxretry = 3/g' /etc/fail2ban/jail.local
+  service fail2ban restart
+fi
