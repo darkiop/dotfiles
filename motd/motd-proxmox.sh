d667282fda836b23d79c9119cdf1254a1eb9cc0a
Thorsten Walk <darkiop@gmail.com>
2021-07-03 10:25:01 +0200

    motd proxmox

--- motd/motd-proxmox.sh
+++ motd/motd-proxmox.sh
@@ -2,11 +2,11 @@
 
 echo -e "$blue_color"Proxmox Version"$close_color   `echo -e "$green_color$(pveversion)$close_color"`"
 echo
-echo -e "$light_blue_color"LXC Container"$close_color"
+echo -e "$light_blue_color"LXC"$close_color"
 echo
 echo -e $yellow_color"`pct list | sed -e 's/^[ \t]*//' | sed '1d' | sed "s,.*,$(echo -e $yellow_color)&$close_color," | sed "s,running,$(echo -e $green_color)&$close_color," | sed "s,stopped,$(echo -e $red_color)&$close_color,"`"$close_color | awk '{print $1,$3,$2}' | sed -e's/  */ /g'
 echo
-echo -e "$light_blue_color"Virtual Machines"$close_color"
+echo -e "$light_blue_color"VM"$close_color"
 echo
 echo -e $yellow_color"`qm list | sed -e 's/^[ \t]*//' | sed '1d' | sed "s,.*,$(echo -e $yellow_color)&$close_color," | sed "s,running,$(echo -e $green_color)&$close_color," | sed "s,stopped,$(echo -e $red_color)&$close_color,"`"$close_color | awk '{print $1,$2,$3}' | sed -e's/  */ /g'
 
