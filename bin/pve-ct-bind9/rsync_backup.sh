b50600bd128cee243d7ef2f5aff5619cddbbcadf
Thorsten Walk <darkiop@gmail.com>
2021-07-27 07:01:20 +0200

    rsync backup

--- bin/pve-ct-bind9/rsync_backup.sh
+++ bin/pve-ct-bind9/rsync_backup.sh
@@ -1,64 +1,76 @@
 #!/bin/bash
 
 DATE=$(date +%Y%m%d_%H%M%S)
-BACKUP_NAME="bind9"
+BACKUP_NAME="test"
 BACKUP_COMPLETE_NAME="$BACKUP_NAME-$DATE"
 BACKUP_OBJECTS="/etc/bind/ /var/cache/bind/"
-BACKUP_LOCAL_TARGET="/home/darkiop/backups/$BACKUP_NAME"
-BACKUP_RSYNC_TARGET="/mnt/odin/backup"
-BACKUPS=`find $BACKUP_LOCAL_TARGET -name "bind9-*.gz" | wc -l | sed 's/\ //g'`
-KEEP=50
+BACKUP_PATH_LOCAL="/home/darkiop/backups/$BACKUP_NAME"
+BACKUP_PATH_REMOTE="/mnt/odin/backup"
+BACKUP_FILES=`find $BACKUP_PATH_LOCAL -name "$BACKUP_NAME-*.gz" | wc -l | sed 's/\ //g'`
+KEEP=8
 IOB_SIMPLEAPI_DOMAIN="pve-vm-iobroker"
 IOB_SIMPLEAPI_PORT="8087"
 OID_EMAIL_TITLE="javascript.0.System.SendeTextperEmailBetreff"
 OID_EMAIL_TEXT="javascript.0.System.SendeTextperEmail"
-EMAIL_TITLE="bind9%20backup"
+EMAIL_TITLE=$BACKUP_NAME"%20backup"
 EMAIL_TEXT_TRUE=$EMAIL_TITLE"%20ausgefuehrt"
 EMAIL_TEXT_FALSE=$EMAIL_TITLE"%20konnte%20nicht%20ausgefuehrt%20werden"
 
-function targz_to_local_target() {
-  tar czf $BACKUP_LOCAL_TARGET/$BACKUP_COMPLETE_NAME.tar.gz $BACKUP_OBJECTS
+function targz_to_local_path() {
+  tar czf $BACKUP_PATH_LOCAL/$BACKUP_COMPLETE_NAME.tar.gz $BACKUP_OBJECTS
 }
 
-function rsync_to_rsync_target() {
-  rsync -avz --delete $BACKUP_LOCAL_TARGET $BACKUP_RSYNC_TARGET
+function rsync_to_remote_path() {
+  rsync -avz --delete $BACKUP_PATH_LOCAL $BACKUP_PATH_REMOTE
 }
 
 function cleanup() {
-  while [ $BACKUPS -ge $KEEP ]
+  while [ $BACKUP_FILES -ge $KEEP ]
   do
-    ls -tr1 $BACKUP_LOCAL_TARGET/$BACKUP_NAME-*.gz | head -n 1 | xargs rm -f 
-    echo "deleting old backup files [$BACKUPS] ..."
-    BACKUPS=`expr $BACKUPS - 1` 
+    ls -tr1 $BACKUP_PATH_LOCAL/$BACKUP_NAME-*.gz | head -n 1 | xargs rm -f 
+    echo "deleting old backup files [$BACKUP_FILES] ..."
+    BACKUP_FILES=`expr $BACKUP_FILES - 1` 
   done
 }
 
 function send_email_with_iobroker() {
   echo "test"
+  # TODO email nur wenn false
+
+  sleep 2
+  curl http://$IOB_SIMPLEAPI_DOMAIN:$IOB_SIMPLEAPI_PORT/set/$OID_EMAIL_TITLE?value=$EMAIL_TITLE >/dev/null 2>&1
+  sleep 2
+  curl http://$IOB_SIMPLEAPI_DOMAIN:$IOB_SIMPLEAPI_PORT/set/$OID_EMAIL_TEXT?value=$EMAIL_TEXT_TRUE >/dev/null 2>&1
+
 }
 
+# check if local path exists, and if not create it
+if [ ! -d $BACKUP_PATH_LOCAL ]; then
+  mkdir $BACKUP_PATH_LOCAL
+fi
+
 # check mountpoint
-if mountpoint -q $BACKUP_RSYNC_TARGET
+if mountpoint -q $BACKUP_PATH_REMOTE
 then
   # mountpoint exists, run rsync
   cleanup
-  targz_to_local_target
-  rsync_to_rsync_target
+  targz_to_local_path
+  rsync_to_remote_path
   sleep 2
   curl http://$IOB_SIMPLEAPI_DOMAIN:$IOB_SIMPLEAPI_PORT/set/$OID_EMAIL_TITLE?value=$EMAIL_TITLE >/dev/null 2>&1
   sleep 2
   curl http://$IOB_SIMPLEAPI_DOMAIN:$IOB_SIMPLEAPI_PORT/set/$OID_EMAIL_TEXT?value=$EMAIL_TEXT_TRUE >/dev/null 2>&1
 else
   # try to mount
-  mount $BACKUP_RSYNC_TARGET
+  mount $BACKUP_PATH_REMOTE
   sleep 2
   # check mountpoint again
-  if mountpoint -q $BACKUP_RSYNC_TARGET
+  if mountpoint -q $BACKUP_PATH_REMOTE
   then
     # mountpoint exists, run rsync
     cleanup
-    targz_to_local_target
-    rsync_to_rsync_target
+    targz_to_local_path
+    rsync_to_remote_path
   else
     # exit
     curl http://$IOB_SIMPLEAPI_DOMAIN:$IOB_SIMPLEAPI_PORT/set/$OID_EMAIL_TITLE?value=$EMAIL_TITLE >/dev/null 2>&1
