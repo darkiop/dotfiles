2c5905f4d5e378ac66c0656cbd7174782f88c456
Thorsten Walk <darkiop@gmail.com>
2021-08-14 12:37:25 +0200

    check-vm

--- bin/proxmox/check-vm.sh
+++ bin/proxmox/check-vm.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
-ping -c 1 10.4.1.37 &> /dev/null && echo "Ping OK - VM 307" || /usr/sbin/qm stop 307 && /usr/sbin/qm start 307 && echo "No response: Restarting VM 307"
+# -------------------------------------------------------------
+# check if user is root and if not exit
+# -------------------------------------------------------------
+function check_if_user_is_root() {
+  if [ "${EUID}" -ne 0 ]; then
+    echo "You need to run this as root. Exit."
+    exit 1
+  fi
+}
+
+check_if_user_is_root
+
+if [ "$#" -ne 2 ]; then
+  echo "Usage: check-vm.sh <VM-ID> <VM-IP>"
+  exit 1
+fi
+
+VM=$1
+IP=$2
+
+ping -c 1 $IP &> /dev/null && echo "Ping OK - VM $VM" || /usr/sbin/qm stop $VM && /usr/sbin/qm start $VM && echo "No response: Restarting VM $VM"
