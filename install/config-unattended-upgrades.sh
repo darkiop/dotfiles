f5af8085dfecde37d6a8c5ebaae85f1a6753c16c
Thorsten Walk <darkiop@gmail.com>
2021-08-14 13:43:46 +0200

    WIP unattended-upgrades

--- install/config-unattended-upgrades.sh
+++ install/config-unattended-upgrades.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#
+# work in progress !
+
+DATE=$(date +%Y%m%d_%H%M)
 
 # check if root
 if [ "${EUID}" -ne 0 ]; then
@@ -16,7 +20,24 @@ fi
 if [ -f /etc/apt/apt.conf.d/20auto-upgrades ]; then
   sed -i 's/^APT::Periodic::Update-Package-Lists "1";/APT::Periodic::Update-Package-Lists "always";/g' /etc/apt/apt.conf.d/20auto-upgrades
   sed -i 's/^APT::Periodic::Unattended-Upgrade "1";/APT::Periodic::Unattended-Upgrade "always";/g' /etc/apt/apt.conf.d/20auto-upgrades
+  echo
   echo "/etc/apt/apt.conf.d/20auto-upgrades:"
+  echo
   /bin/cat /etc/apt/apt.conf.d/20auto-upgrades
   echo
 fi
+
+# /etc/apt/apt.conf.d/50unattended-upgrades
+
+# backup original file
+if [ -f /etc/apt/apt.conf.d/50unattended-upgrades ]; then
+  cp /etc/apt/apt.conf.d/50unattended-upgrades /etc/apt/apt.conf.d/50unattended-upgrades.$DATE.bak
+fi
+
+# if ubuntu
+# //  "${distro_id}:${distro_codename}-updates";
+# //  "${distro_id}:${distro_codename}-proposed";
+
+# if debian
+# //      "origin=Debian,codename=${distro_codename}-updates";
+# //      "origin=Debian,codename=${distro_codename}-proposed-updates";
