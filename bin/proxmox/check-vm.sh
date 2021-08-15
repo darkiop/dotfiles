c0edea7e713ff98148bb0e52f0463be6d2dcd60f
Thorsten Walk <darkiop@gmail.com>
2021-08-15 08:05:13 +0200

    check-vm

--- bin/proxmox/check-vm.sh
+++ bin/proxmox/check-vm.sh
@@ -1,23 +1,46 @@
 #!/bin/bash
 
-# -------------------------------------------------------------
-# check if user is root and if not exit
-# -------------------------------------------------------------
-function check_if_user_is_root() {
-  if [ "${EUID}" -ne 0 ]; then
-    echo "You need to run this as root. Exit."
-    exit 1
-  fi
-}
+DATE=$(date +%Y-%m-%d_%H:%M:%S)
+LOGFILE="/var/log/check_vm.log"
 
-check_if_user_is_root
+# check if root
+if [ "${EUID}" -ne 0 ]; then
+  echo "You need to run this as root. Exit."
+  exit 1
+fi
 
+# usage
 if [ "$#" -ne 2 ]; then
   echo "Usage: check-vm.sh <VM-ID> <VM-IP>"
   exit 1
 fi
 
-VM=$1
-IP=$2
+# validate vm-id
+if [[ $1 =~ ((^[0-9]{3}$)) ]]; then
+  VM=$1
+else
+  echo "Parameter 1 is not a VM-ID."
+  exit 1
+fi
 
-ping -c 1 $IP &> /dev/null && echo "Ping OK - VM $VM" || /usr/sbin/qm stop $VM && /usr/sbin/qm start $VM && echo "No response: Restarting VM $VM"
+# validate ip
+if [[ $2 =~ (([01]{,1}[0-9]{1,2}|2[0-4][0-9]|25[0-5])\.([01]{,1}[0-9]{1,2}|2[0-4][0-9]|25[0-5])\.([01]{,1}[0-9]{1,2}|2[0-4][0-9]|25[0-5])\.([01]{,1}[0-9]{1,2}|2[0-4][0-9]|25[0-5]))$ ]]; then
+  IP=$2
+else
+  echo "Parameter 2 is not a valid IP."
+  exit 1
+fi
+
+# check vm with fping
+fping -c1 -t1000 $IP &>/dev/null
+if [ "$?" = 0 ]; then
+  echo "VM $VM is online"
+  echo $DATE" - VM $VM is online" >> ${LOGFILE}
+else
+  echo "VM $VM is offline. Restarting VM $VM"
+  echo $DATE" - VM $VM offline. Restarting VM $VM" >> ${LOGFILE}
+  /usr/sbin/qm stop $VM
+  /usr/sbin/qm start $VM
+fi
+
+# EOF
