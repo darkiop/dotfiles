925be0621880e84eb4092b6f0d17fae3bca754f0
Thorsten Walk <darkiop@gmail.com>
2021-08-16 12:20:08 +0200

    WIP install-sshfs

--- install/install-sshfs.sh
+++ install/install-sshfs.sh
@@ -4,8 +4,8 @@
 
 SSHFSDIR=$HOME'/sshfs'
 SSHFSOPTIONS="uid=1000,gid=1000,umask=0,IdentityFile=/home/darkiop/.ssh/id_rsa,nonempty"
-#HOSTS=(pve01 pve-ct-adguard pve-ct-grafana pve-ct-checkmk pve-ct-mariadb pve-ct-wireguard pve-ct-bind9 pve-ct-heimdall pve-ct-bitwarden pve-ct-cockpit pve-vm-raspberyymatic pve-vm-iobroker pve-vm-pbs odin udmp)
 
+#HOSTS=(pve01 pve-ct-adguard pve-ct-grafana pve-ct-checkmk pve-ct-mariadb pve-ct-wireguard pve-ct-bind9 pve-ct-heimdall pve-ct-bitwarden pve-ct-cockpit pve-vm-raspberyymatic pve-vm-iobroker pve-vm-pbs odin udmp)
 HOSTS=(pve01 pve-vm-iobroker)
 FOLDERS=(pve01 pve01/home-darkiop pve-vm-iobroker pve-vm-iobroker/home-darkiop pve-vm-iobroker/opt-iobroker)
 
@@ -14,20 +14,25 @@ if [ "" = "$(dpkg-query -W --showformat='${Status}\n' sshfs|grep "install ok ins
   sudo apt install sshfs -y
 fi
 
+# install mountpoint
+if [ "" = "$(dpkg-query -W --showformat='${Status}\n' mountpoint|grep "install ok installed")" ]; then
+  sudo apt install mountpoint -y
+fi
+
 # create sshfs dir
 if [ ! -d $SSHFSDIR ]; then
   mkdir $SSHFSDIR
 fi
 
-# create folder for sshfs
+# create folders for sshfs
 let i=1
 for dir in "${FOLDERS[@]}"; do
   if [ $dir != $HOSTNAME ]; then
-    mkdir -p $SSHFSDIR/$dir
+    mkdir -v -p $SSHFSDIR/$dir
   fi
 done
 
-# create local ssh key
+# create a local ssh key
 if [ ! -f $HOME/.ssh/id_rsa ]; then
   ssh-keygen -t rsa -b 4096
 fi
@@ -40,19 +45,27 @@ for remotessh in "${HOSTS[@]}"; do
 done
 
 # mount
-function connectSSHFS() {
-  # TODO validate parameters
+function mountSSHFS() {
+  # TODO check if already mounted
+  # TODO $1: check if host is online
+  # TODO $2: check if remote dir exists
+  # TODO $3: check if local dir exists
   if [ "$#" -ne 3 ]; then
-    echo "Usage: Parameter-1: user@host | Parameter-2: SOURCE-DIR, e.g. /home/darkiop | Parameter-3: TARGET-DIR, e.g. host/home-darkiop"
+    echo "Usage: Parameter-1: host| Parameter-2: remote-dir, e.g. /home/darkiop | Parameter-3: TARGET-DIR, e.g. home-darkiop"
     exit 1
   fi
-  sudo sshfs -o $SSHFSOPTIONS $1:$2/ $SSHFSDIR/$3
+
+  if mountpoint -q $SSHFSDIR/$1/$3; then
+    echo "mount exist, exit."
+    exit 1
+  fi
+
+  sshfs -o $SSHFSOPTIONS $USER@$1:$2/ $SSHFSDIR/$1/$3
 }
 
-connectSSHFS darkiop@pve01 /home/darkiop pve01/home-darkiop
-connectSSHFS darkiop@pve-vm-iobroker /home/darkiop pve-vm-iobroker/home-darkiop
-connectSSHFS darkiop@pve-vm-iobroker /opt/iobroker pve-vm-iobroker/opt-iobroker
+mountSSHFS pve01 /home/darkiop home-darkiop
+mountSSHFS pve-vm-iobroker /home/darkiop home-darkiop
+mountSSHFS pve-vm-iobroker /opt/iobroker opt-iobroker
 
+# debug: show result
 tree -L 3 $SSHFSDIR
-
-#sshfs -o $SSHFSOPTIONS darkiop@pve-vm-iobroker:/opt/iobroker/ /home/darkiop/sshfs/pve-vm-iobroker/opt-iobroker
