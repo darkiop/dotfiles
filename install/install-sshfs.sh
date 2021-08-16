ba81e645d6baacf2bdde43a2fbd37907a2fbaa37
Thorsten Walk <darkiop@gmail.com>
2021-08-16 09:28:43 +0200

    wip install-sshfs

--- install/install-sshfs.sh
+++ install/install-sshfs.sh
@@ -3,9 +3,11 @@
 # WORK IN PROGRESS
 
 SSHFSDIR=$HOME'/sshfs'
-SSHFSOPTIONS="uid=1000,gid=100,umask=0,IdentityFile=/home/darkiop/.ssh/id_rsa,nonempty"
+SSHFSOPTIONS="uid=1000,gid=1000,umask=0,IdentityFile=/home/darkiop/.ssh/id_rsa,nonempty"
 #HOSTS=(pve01 pve-ct-adguard pve-ct-grafana pve-ct-checkmk pve-ct-mariadb pve-ct-wireguard pve-ct-bind9 pve-ct-heimdall pve-ct-bitwarden pve-ct-cockpit pve-vm-raspberyymatic pve-vm-iobroker pve-vm-pbs odin udmp)
-HOSTS=(pve-ct-cockpit pve-ct-wireguard)
+
+HOSTS=(pve01 pve-vm-iobroker)
+FOLDERS=(pve01 pve01/home-darkiop pve-vm-iobroker pve-vm-iobroker/home-darkiop pve-vm-iobroker/opt-iobroker)
 
 # install sshfs
 if [ "" = "$(dpkg-query -W --showformat='${Status}\n' sshfs|grep "install ok installed")" ]; then
@@ -19,7 +21,7 @@ fi
 
 # create folder for sshfs
 let i=1
-for dir in "${HOSTS[@]}"; do
+for dir in "${FOLDERS[@]}"; do
   if [ $dir != $HOSTNAME ]; then
     mkdir -p $SSHFSDIR/$dir
   fi
@@ -33,10 +35,24 @@ fi
 # copy public ssh key to remote hosts
 for remotessh in "${HOSTS[@]}"; do
   if [ $remotessh != $HOSTNAME ]; then
-    ssh-copy-id -i $HOME/.ssh/id_rsa.pub $remotessh
+    ssh-copy-id -i $HOME/.ssh/id_rsa.pub $remotessh &>/dev/null
   fi
 done
 
 # mount
-# wip
-sshfs -o $SSHFSOPTIONS darkiop@pve-ct-wireguard:/home/darkiop/ /home/darkiop/sshfs/pve-ct-wireguard/home-darkiop
+function connectSSHFS() {
+  # TODO validate parameters
+  if [ "$#" -ne 3 ]; then
+    echo "Usage: Parameter-1: user@host | Parameter-2: SOURCE-DIR, e.g. /home/darkiop | Parameter-3: TARGET-DIR, e.g. host/home-darkiop"
+    exit 1
+  fi
+  sudo sshfs -o $SSHFSOPTIONS $1:$2/ $SSHFSDIR/$3
+}
+
+connectSSHFS darkiop@pve01 /home/darkiop pve01/home-darkiop
+connectSSHFS darkiop@pve-vm-iobroker /home/darkiop pve-vm-iobroker/home-darkiop
+connectSSHFS darkiop@pve-vm-iobroker /opt/iobroker pve-vm-iobroker/opt-iobroker
+
+tree -L 3 $SSHFSDIR
+
+#sshfs -o $SSHFSOPTIONS darkiop@pve-vm-iobroker:/opt/iobroker/ /home/darkiop/sshfs/pve-vm-iobroker/opt-iobroker
