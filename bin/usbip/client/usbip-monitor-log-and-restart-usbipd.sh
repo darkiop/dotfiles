748149df45917b5f8ca171b837fdaf97f87c3968
Thorsten Walk <darkiop@gmail.com>
2021-09-27 22:03:43 +0200

    usbip

--- bin/usbip/client/usbip-monitor-log-and-restart-usbipd.sh
+++ bin/usbip/client/usbip-monitor-log-and-restart-usbipd.sh
@@ -4,6 +4,6 @@ tail -fn0 /opt/iobroker/log/iobroker.current.log | \
 while read line ; do
   echo "$line" | grep "No or too long answer from Serial Device after last request"
   if [ $? = 0 ]; then
-    ssh darkiop@loki bash -c "'reboot'"
+    /usr/bin/ssh darkiop@loki bash -c "'sudo /usr/sbin/reboot -f'"
   fi
 done
