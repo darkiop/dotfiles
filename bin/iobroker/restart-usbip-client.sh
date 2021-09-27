382fc3cdef1c19bad5d7c8367f37eb2cfe83060b
Thorsten Walk <darkiop@gmail.com>
2021-09-27 17:18:10 +0200

    usbip client

--- bin/iobroker/restart-usbip-client.sh
+++ bin/iobroker/restart-usbip-client.sh
@@ -8,7 +8,7 @@
 # IgnoreOnIsolate=yes
 #
 # [Service]
-# ExecStart=/root/dotfiles/bin/iobroker/restart-usbip-clientsh
+# ExecStart=/root/dotfiles/bin/iobroker/restart-usbip-client.sh
 # ExecStop=
 # Type=oneshot
 # RemainAfterExit=true
@@ -16,4 +16,4 @@
 # [Install]
 # WantedBy=basic.target
 
-ssh darkiop@pve-vm-iobroker bash -c "'sudo service usbip-client start'"
+ssh darkiop@pve-vm-iobroker bash -c "'sudo service usbip-client restart'"
