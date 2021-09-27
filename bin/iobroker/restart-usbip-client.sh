6ba59a2cec667f23838399be40a1f1a1650f9cab
Thorsten Walk <darkiop@gmail.com>
2021-09-27 18:04:03 +0200

    usbip client restart

--- bin/iobroker/restart-usbip-client.sh
+++ bin/iobroker/restart-usbip-client.sh
@@ -1,19 +1,2 @@
 #!/bin/bash
-# /etc/systemd/system/getty.target.wants/restart-usbip-client-iobroker.service
-#
-# [Unit]
-# After=systemd-user-sessions.service
-# After=rc-local.service
-# Before=getty.target
-# IgnoreOnIsolate=yes
-#
-# [Service]
-# ExecStart=/root/dotfiles/bin/iobroker/restart-usbip-client.sh
-# ExecStop=
-# Type=oneshot
-# RemainAfterExit=true
-#
-# [Install]
-# WantedBy=basic.target
-
 ssh darkiop@pve-vm-iobroker bash -c "'sudo service usbip-client restart'"
