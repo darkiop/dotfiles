30afcf74d9cbb735c1ddcb000b749daf8e9868a6
Thorsten Walk <darkiop@gmail.com>
2021-10-09 10:52:25 +0200

    motd

--- motd/motd.sh
+++ motd/motd.sh
@@ -3,30 +3,26 @@
 # modified by thwalk
 
 # uptime
-upSeconds="$(/usr/bin/cut -d. -f1 /proc/uptime)"
-secs=$((${upSeconds}%60))
-mins=$((${upSeconds}/60%60))
-hours=$((${upSeconds}/3600%24))
-days=$((${upSeconds}/86400))
-UP=`printf "%d days, %02dh:%02dm:%02ds" "$days" "$hours" "$mins" "$secs"`
+UPTIME="$(/usr/bin/cut -d. -f1 /proc/uptime)"
+UPTIME_SECONDS=$((${UPTIME}%60))
+UPTIME_MIN=$((${UPTIME}/60%60))
+UPTIME_HOURS=$((${UPTIME}/3600%24))
+UPTIME_DAYS=$((${UPTIME}/86400))
+UPTIME_TEXT=`printf "%d UPTIME_DAYS, %02dh:%02dm:%02ds" "$UPTIME_DAYS" "$UPTIME_HOURS" "$UPTIME_MIN" "$UPTIME_SECONDS"`
 
 # size of /
-root_usage=$(df -h / | awk '/\// {print $(NF-1)}' | sed 's/%//g')
-root_usage_gb=$(df -h / | awk '/\// {print $(NF-3)}')
-root_total=$(df -h / | awk '/\// {print $(NF-4)}')
+USAGE_ROOT=$(df -h / | awk '/\// {print $(NF-1)}' | sed 's/%//g')
+USAGE_ROOT_GB=$(df -h / | awk '/\// {print $(NF-3)}')
+USAGE_ROOT_TOTAL=$(df -h / | awk '/\// {print $(NF-4)}')
 
-# size of /home
-if [ -d /home ]; then
-  home_usage=$(df -h /home | awk '/\// {print $(NF-1)}' | sed 's/%//g')
-  home_usage_gb=$(df -h /home | awk '/\// {print $(NF-3)}')
-  home_total=$(df -h /home | awk '/\// {print $(NF-4)}')
-fi
+# home-size
+HOME_USAGE=$(du -h | tail -n 1 | awk '{print $1}')
 
 # get hostname
-get_host_name=$(hostname)
+HOSTNAME=$(hostname)
 
 # get os version & ip & cputemp
-case $get_host_name in
+case $HOSTNAME in
   (odin)
     get_plat_data="Synology DSM "$(cat /etc.defaults/VERSION | grep productversion | awk -F'=' '{print $2}' | sed 's/"//' | sed 's/"//')
     get_cpu_temp=$(($(cat /sys/class/hwmon/hwmon0/temp1_input)/1000))"°C"
@@ -39,32 +35,30 @@ case $get_host_name in
 esac
 
 # cpu load av
-get_os_load_1=$(cat /proc/loadavg | awk '{ print $1 }')
-get_os_load_5=$(cat /proc/loadavg | awk '{ print $2 }')
-get_os_load_15=$(cat /proc/loadavg | awk '{ print $3 }')
-get_os_loadavg=`echo -e "$get_os_load_1" / " $get_os_load_5 $get_os_load_15"`
-get_proc_ps=$(ps -Afl | wc -l)
-get_swap=$(free -m | tail -n 1 | awk {'print $3'})
+LOAD1=$(cat /proc/loadavg | awk '{ print $1 }')
+LOAD5=$(cat /proc/loadavg | awk '{ print $2 }')
+LOAD15=$(cat /proc/loadavg | awk '{ print $3 }')
+SHOW_LOAD_AVG=`echo -e "$LOAD1" / " $LOAD5 $LOAD15"`
 
 # set COLOR_CLOSE
-case $get_host_name in
+case $HOSTNAME in
   (odin)  COLOR_CLOSE="";;
   (*)     COLOR_CLOSE="$(tput sgr0)";;
 esac
 
 # set title of terminal
-trap 'echo -ne "\033]0;${USER}@${get_host_name}:${PWD}\007"' DEBUG
+trap 'echo -ne "\033]0;${USER}@${HOSTNAME}:${PWD}\007"' DEBUG
 
 # read task file
-if [ -f ~/dotfiles/motd/tasks-${get_host_name} ]; then
-  tasks="$(cat ~/dotfiles/motd/tasks-${get_host_name})"
+if [ -f ~/dotfiles/motd/tasks-${HOSTNAME} ]; then
+  TASKS="$(cat ~/dotfiles/motd/tasks-${HOSTNAME})"
 else
-  tasks="$(cat ~/dotfiles/motd/tasks)"
+  TASKS="$(cat ~/dotfiles/motd/tasks)"
 fi
 
 # use toilet for title of motd
 # show all available fonts: https://gist.github.com/itzg/b889534a029855c018813458ff24f23c
-case ${get_host_name} in
+case ${HOSTNAME} in
   (odin)
     clear
     echo -e "$COLOR_YELLOW"
@@ -79,49 +73,40 @@ EOF
   (*)
     if [ -x "$(command -v toilet)" ]; then
       echo -e "$COLOR_YELLOW"
-      toilet -f smblock -w 150 $get_host_name
+      toilet -f smblock -w 150 $HOSTNAME
       echo -e "$COLOR_CLOSE"
     fi
   ;;
 esac
 
-# echo infos
-echo -e "$COLOR_BLUE"ip"$COLOR_CLOSE                `echo -e "$COLOR_GREEN$get_ip_host$COLOR_CLOSE"`
-$COLOR_BLUE"tasks"$COLOR_CLOSE             `echo -e "$COLOR_GREEN$tasks$COLOR_CLOSE"`
-$COLOR_BLUE"load"$COLOR_CLOSE              `echo -e "$COLOR_GREEN$get_os_load_1$COLOR_CLOSE" / "$COLOR_GREEN$get_os_load_5$COLOR_CLOSE" / "$COLOR_GREEN$get_os_load_15$COLOR_CLOSE"`
-$COLOR_BLUE"uptime"$COLOR_CLOSE            `echo -e "$COLOR_GREEN$UP$COLOR_CLOSE"`
-$COLOR_BLUE"os"$COLOR_CLOSE                `echo -e "$COLOR_GREEN$get_plat_data$COLOR_CLOSE"`
-$COLOR_BLUE"usage of /"$COLOR_CLOSE        `echo -e "$COLOR_GREEN$root_usage_gb$COLOR_CLOSE"` "of" `echo -e "$COLOR_GREEN$root_total$COLOR_CLOSE"` ($root_usage%)
-$COLOR_BLUE"usage of /home"$COLOR_CLOSE    `echo -e "$COLOR_GREEN$home_usage_gb$COLOR_CLOSE"` "of" `echo -e "$COLOR_GREEN$home_total$COLOR_CLOSE"` ($home_usage%)"
+# BUILD THE MOTD OUTPUT
+echo -e "$COLOR_BLUE"ip"$COLOR_CLOSE        `echo -e "$COLOR_GREEN$get_ip_host$COLOR_CLOSE"`
+$COLOR_BLUE"tasks"$COLOR_CLOSE     `echo -e "$COLOR_GREEN$TASKS$COLOR_CLOSE"`
+$COLOR_BLUE"load"$COLOR_CLOSE      `echo -e "$COLOR_GREEN$LOAD1$COLOR_CLOSE" / "$COLOR_GREEN$LOAD5$COLOR_CLOSE" / "$COLOR_GREEN$LOAD15$COLOR_CLOSE"`
+$COLOR_BLUE"uptime"$COLOR_CLOSE    `echo -e "$COLOR_GREEN$UPTIME_TEXT$COLOR_CLOSE"`
+$COLOR_BLUE"os"$COLOR_CLOSE        `echo -e "$COLOR_GREEN$get_plat_data$COLOR_CLOSE"`
+$COLOR_BLUE"/"$COLOR_CLOSE         `echo -e "$COLOR_GREEN$USAGE_ROOT_GB$COLOR_CLOSE"` "of" `echo -e "$COLOR_GREEN$USAGE_ROOT_TOTAL$COLOR_CLOSE"` ($USAGE_ROOT%)
+$COLOR_BLUE"/home"$COLOR_CLOSE     `echo -e "$COLOR_GREEN$HOME_USAGE$COLOR_CLOSE"`"
 
-# special motd for proxmox
+# motd for proxmox
 if [ -x /usr/bin/pveversion ]; then
   source ~/dotfiles/motd/motd-proxmox.sh
 fi
 
-# special motd for redis
-#if pgrep -x "redis-server" >/dev/null
-#then
-#  source ~/dotfiles/motd/motd-redis.sh
-#fi
-
-# special motd by hostname
-if [ -f ~/dotfiles/motd/motd-$get_host_name.sh ]; then
-  source ~/dotfiles/motd/motd-$get_host_name.sh
+# motd by hostname
+if [ -f ~/dotfiles/motd/motd-$HOSTNAME.sh ]; then
+  source ~/dotfiles/motd/motd-$HOSTNAME.sh
 else
   echo
 fi
 
 # show updates
 if [ $MOTD_SHOW_APT_UPDATES=="y" ]; then
-  if [ $(which apt) ]; then
-    echo -e "$COLOR_LIGHT_BLUE"Checking for updates ..."$COLOR_CLOSE"
-    updates="$(apt-get -s dist-upgrade | awk '/^Inst/ { print $2 }')"
-    updatesText="$COLOR_GREEN$COLOR_CLOSE $COLOR_YELLOW$updates$COLOR_CLOSE"
-    if [ "$(which apt-get)" ]; then
-      echo
-      echo -e $COLOR_YELLOW"`apt-get -s -o Debug::NoLocking=true upgrade | grep ^Inst | wc -l` "$COLOR_GREEN"updates to install"$COLOR_CLOSE$updatesText
-    fi
-  fi
+  echo -e "$COLOR_LIGHT_BLUE"APT"$COLOR_CLOSE"
+  updates_count=$(</usr/local/share/dotfiles/apt-updates-count)
+  updates_packages=$(</usr/local/share/dotfiles/apt-updates-packages)
+  echo
+  echo -e $COLOR_YELLOW$updates_count$COLOR_GREEN" updates to install "$COLOR_YELLOW$updates_packages$COLOR_CLOSE
 fi
+
 # EOF
