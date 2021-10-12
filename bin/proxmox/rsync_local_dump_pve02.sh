02558125005219bc4b3a99180e20f97c2f8ba77a
Thorsten Walk <darkiop@gmail.com>
2021-10-12 05:56:49 +0200

    pve backup

--- bin/proxmox/rsync_local_dump_pve02.sh
+++ bin/proxmox/rsync_local_dump_pve02.sh
@@ -2,6 +2,8 @@
 #
 # 1 Uhr     / 7 Uhr     / 13 Uhr     / 19 Uhr
 # 0 1 * * * / 0 7 * * * / 0 13 * * * / 0 19 * * *
+#
+# 0 19 * * * /root/dotfiles/bin/proxmox/rsync_local_dump_pve02.sh
 
 BACKUP_NAME="local-dumps-pve02"
 BACKUP_PATH_LOCAL="/var/lib/vz/dump/"
