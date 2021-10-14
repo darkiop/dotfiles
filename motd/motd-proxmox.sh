0534fb586cc35ec388423c17f4520331d72fd94b
Thorsten Walk <darkiop@gmail.com>
2021-10-14 08:02:39 +0200

    motd

--- motd/motd-proxmox.sh
+++ motd/motd-proxmox.sh
@@ -23,18 +23,18 @@ else
   glusterfs_arbiter_colored=$COLOR_RED$glusterfs_arbiter$COLOR_CLOSE$COLOR_YELLOW
 fi
 
-echo -e "$COLOR_BLUE"Proxmox"$COLOR_CLOSE   `echo -e "$COLOR_GREEN$(pveversion)$COLOR_CLOSE"`"
+echo -e "$COLOR_BLUE" Proxmox"$COLOR_CLOSE   `echo -e "$COLOR_GREEN$(pveversion)$COLOR_CLOSE"`"
 echo
-echo -e "$COLOR_LIGHT_BLUE"GlusterFS"$COLOR_CLOSE"
+echo -e "$COLOR_LIGHT_BLUE" GlusterFS"$COLOR_CLOSE"
 echo
-echo -e $COLOR_YELLOW"Brick-1: "$glusterfs_brick1_colored" Brick-2: "$glusterfs_brick2_colored" Arbiter: "$glusterfs_arbiter_colored$COLOR_CLOSE
+echo -e $COLOR_YELLOW" Brick-1: "$glusterfs_brick1_colored" Brick-2: "$glusterfs_brick2_colored" Arbiter: "$glusterfs_arbiter_colored$COLOR_CLOSE
 echo
-echo -e "$COLOR_LIGHT_BLUE"LXC"$COLOR_CLOSE"
+echo -e "$COLOR_LIGHT_BLUE" LXC"$COLOR_CLOSE"
 echo
-echo -e $COLOR_YELLOW"`pct list | sed -e 's/^[ \t]*//' | sed '1d' | sed "s,.*,$(echo -e $COLOR_YELLOW)&$COLOR_CLOSE," | sed "s,running,$(echo -e $COLOR_GREEN)&$COLOR_CLOSE," | sed "s,stopped,$(echo -e $COLOR_RED)&$COLOR_CLOSE,"`"$COLOR_CLOSE | awk '{print $1,$3,$2}' | sed -e's/  */ /g'
+echo -e $COLOR_YELLOW" `pct list | sed -e 's/^[ \t]*//' | sed '1d' | sed "s,.*,$(echo -e $COLOR_YELLOW)&$COLOR_CLOSE," | sed "s,running,$(echo -e $COLOR_GREEN)&$COLOR_CLOSE," | sed "s,stopped,$(echo -e $COLOR_RED)&$COLOR_CLOSE,"`"$COLOR_CLOSE | awk '{print $1,$3,$2}' | sed -e's/  */ /g'
 echo
-echo -e "$COLOR_LIGHT_BLUE"VM"$COLOR_CLOSE"
+echo -e "$COLOR_LIGHT_BLUE" VM"$COLOR_CLOSE"
 echo
-echo -e $COLOR_YELLOW"`qm list | sed -e 's/^[ \t]*//' | sed '1d' | sed "s,.*,$(echo -e $COLOR_YELLOW)&$COLOR_CLOSE," | sed "s,running,$(echo -e $COLOR_GREEN)&$COLOR_CLOSE," | sed "s,stopped,$(echo -e $COLOR_RED)&$COLOR_CLOSE,"`"$COLOR_CLOSE | awk '{print $1,$2,$3}' | sed -e's/  */ /g'
+echo -e $COLOR_YELLOW" `qm list | sed -e 's/^[ \t]*//' | sed '1d' | sed "s,.*,$(echo -e $COLOR_YELLOW)&$COLOR_CLOSE," | sed "s,running,$(echo -e $COLOR_GREEN)&$COLOR_CLOSE," | sed "s,stopped,$(echo -e $COLOR_RED)&$COLOR_CLOSE,"`"$COLOR_CLOSE | awk '{print $1,$2,$3}' | sed -e's/  */ /g'
 
 #EOF
