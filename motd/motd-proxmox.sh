48fed0aa96ad44d7a1083851e01fc2f6b1c38f81
Thorsten Walk <darkiop@gmail.com>
2021-11-01 12:20:59 +0100

    cleanup

--- motd/motd-proxmox.sh
+++ motd/motd-proxmox.sh
@@ -23,7 +23,7 @@ else
   glusterfs_arbiter_colored=$COLOR_RED$glusterfs_arbiter$COLOR_CLOSE$COLOR_YELLOW
 fi
 
-echo -e " "$COLOR_BLUE"Proxmox"$COLOR_CLOSE   `echo -e "$COLOR_GREEN$(pveversion)$COLOR_CLOSE"`
+echo -e " "$COLOR_BLUE"Proxmox  "$COLOR_CLOSE`echo -e "$COLOR_GREEN$(pveversion)$COLOR_CLOSE"`
 echo
 echo -e " "$COLOR_LIGHT_BLUE"GlusterFS"$COLOR_CLOSE
 echo
@@ -34,7 +34,12 @@ echo
 echo -e " "$COLOR_YELLOW"`pct list | sed -e 's/^[ \t]*//' | sed '1d' | sed "s,.*,$(echo -e $COLOR_YELLOW)&$COLOR_CLOSE," | sed "s,running,$(echo -e $COLOR_GREEN)&$COLOR_CLOSE," | sed "s,stopped,$(echo -e $COLOR_RED)&$COLOR_CLOSE,"`"$COLOR_CLOSE | awk '{print $1,$3,$2}' | sed -e's/  */ /g' | sed 's/^/ /'
 echo
 echo -e " "$COLOR_LIGHT_BLUE"VM"$COLOR_CLOSE
+if [ $(qm list | sed '1d' | wc -l) -gt 0 ]; then
 echo
 echo -e " "$COLOR_YELLOW"`qm list | sed -e 's/^[ \t]*//' | sed '1d' | sed "s,.*,$(echo -e $COLOR_YELLOW)&$COLOR_CLOSE," | sed "s,running,$(echo -e $COLOR_GREEN)&$COLOR_CLOSE," | sed "s,stopped,$(echo -e $COLOR_RED)&$COLOR_CLOSE,"`"$COLOR_CLOSE | awk '{print $1,$2,$3}' | sed -e's/  */ /g' | sed 's/^/ /'
+else
+echo
+echo -e " "$COLOR_YELLOW"No VM is running."$COLOR_CLOSE
+fi
 
 #EOF
