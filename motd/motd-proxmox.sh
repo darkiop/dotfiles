73ad5782c4ef8dece7bbdcc64bc1faf980a7bcc2
Thorsten Walk <darkiop@gmail.com>
2022-02-06 06:50:24 +0100

    motd-proxmox

--- motd/motd-proxmox.sh
+++ motd/motd-proxmox.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
 
 
-#1
-# gstatus -ab | grep -A3 'Group 1' | grep '192.168.1.50' | awk '{print $2}' | sed 's/(//g' | sed 's/)//g'
-# gstatus -ab | grep -A3 'Group 1' | grep '192.168.1.51' | awk '{print $2}' | sed 's/(//g' | sed 's/)//g'
-# gstatus -ab | grep -A3 'Group 1' | grep '192.168.1.40' | awk '{print $2}' | sed 's/(//g' | sed 's/)//g'
+#1   
+# gstatus -a | grep -A3 'Group 1' | grep '192.168.1.50' | awk '{print $2}' | sed 's/(//g' | sed 's/)//g'
+# gstatus -a | grep -A3 'Group 1' | grep '192.168.1.51' | awk '{print $2}' | sed 's/(//g' | sed 's/)//g'
+# gstatus -a | grep -A3 'Group 1' | grep '192.168.1.41' | awk '{print $2}' | sed 's/(//g' | sed 's/)//g'
 
 # gfs status
-#glusterfs_brick1=$(gluster volume heal glusterfs-1-volume info | grep -A 1 '192.168.1.50' | tail -n 1 | awk '{print $2}')
-#glusterfs_brick2=$(gluster volume heal glusterfs-1-volume info | grep -A 1 '192.168.1.51' | tail -n 1 | awk '{print $2}')
-#glusterfs_arbiter=$(gluster volume heal glusterfs-1-volume info | grep -A 1 '192.168.1.40' | tail -n 1 | awk '{print $2}')
+glusterfs_brick1=$(gstatus -ab | grep -A3 'Group 1' | grep '192.168.1.50' | awk '{print $2}' | sed 's/(//g' | sed 's/)//g')
+glusterfs_brick2=$(gstatus -ab | grep -A3 'Group 1' | grep '192.168.1.51' | awk '{print $2}' | sed 's/(//g' | sed 's/)//g')
+glusterfs_arbiter=$(gstatus -ab | grep -A3 'Group 1' | grep '192.168.1.41' | awk '{print $2}' | sed 's/(//g' | sed 's/)//g')
 #
-#if [ $glusterfs_brick1 == "Connected" ]; then
-#  glusterfs_brick1_colored=$COLOR_GREEN$glusterfs_brick1$COLOR_CLOSE$COLOR_YELLOW
-#else
-#  glusterfs_brick1_colored=$COLOR_RED$glusterfs_brick1$COLOR_CLOSE$COLOR_YELLOW
-#fi
-#
-#if [ $glusterfs_brick2 == "Connected" ]; then
-#  glusterfs_brick2_colored=$COLOR_GREEN$glusterfs_brick2$COLOR_CLOSE$COLOR_YELLOW
-#else
-#  glusterfs_brick2_colored=$COLOR_RED$glusterfs_brick2$COLOR_CLOSE$COLOR_YELLOW
-#fi
-#
-#if [ $glusterfs_arbiter == "Connected" ]; then
-#  glusterfs_arbiter_colored=$COLOR_GREEN$glusterfs_arbiter$COLOR_CLOSE$COLOR_YELLOW
-#else
-#  glusterfs_arbiter_colored=$COLOR_RED$glusterfs_arbiter$COLOR_CLOSE$COLOR_YELLOW
-#fi
+if [[ $glusterfs_brick1 = "Online" ]]; then
+  glusterfs_brick1_colored=$COLOR_GREEN$glusterfs_brick1$COLOR_CLOSE$COLOR_YELLOW
+else
+  glusterfs_brick1_colored=$COLOR_RED$glusterfs_brick1$COLOR_CLOSE$COLOR_YELLOW
+fi
 
-echo -e " "$COLOR_BLUE"Proxmox  "$COLOR_CLOSE`echo -e "$COLOR_GREEN$(pveversion)$COLOR_CLOSE"`
+if [[ $glusterfs_brick2 = "Online" ]]; then
+  glusterfs_brick2_colored=$COLOR_GREEN$glusterfs_brick2$COLOR_CLOSE$COLOR_YELLOW
+else
+  glusterfs_brick2_colored=$COLOR_RED$glusterfs_brick2$COLOR_CLOSE$COLOR_YELLOW
+fi
+
+if [[ $glusterfs_arbiter = "Online" ]]; then
+  glusterfs_arbiter_colored=$COLOR_GREEN$glusterfs_arbiter$COLOR_CLOSE$COLOR_YELLOW
+else
+  glusterfs_arbiter_colored=$COLOR_RED$glusterfs_arbiter$COLOR_CLOSE$COLOR_YELLOW
+fi
+
+echo -e " "$COLOR_BLUE"Proxmox   "$COLOR_CLOSE`echo -e "$COLOR_GREEN$(pveversion)$COLOR_CLOSE"`
 echo
 echo -e " "$COLOR_LIGHT_BLUE"GlusterFS"$COLOR_CLOSE
 echo
