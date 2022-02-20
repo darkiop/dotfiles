0a9b0b1fd65253c5a505e8bdb1622b0af1db7a1a
Thorsten Walk <darkiop@gmail.com>
2022-02-20 06:05:11 +0100

    motd proxmox

--- motd/motd-proxmox.sh
+++ motd/motd-proxmox.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-# gfs status
-glusterfs_brick1=$(gstatus -ab | grep -A3 'Group 1' | grep '192.168.1.50' | awk '{print $2}' | sed 's/(//g' | sed 's/)//g')
-glusterfs_brick2=$(gstatus -ab | grep -A3 'Group 1' | grep '192.168.1.51' | awk '{print $2}' | sed 's/(//g' | sed 's/)//g')
-glusterfs_arbiter=$(gstatus -ab | grep -A3 'Group 1' | grep '192.168.1.41' | awk '{print $2}' | sed 's/(//g' | sed 's/)//g')
-gluster_version=$(gluster --version | head -n1 | awk '{print $2}')
+if [ -x /usr/local/bin/gstatus ]; then
+  glusterfs_brick1=$(gstatus -ab | grep -A3 'Group 1' | grep '192.168.1.50' | awk '{print $2}' | sed 's/(//g' | sed 's/)//g')
+  glusterfs_brick2=$(gstatus -ab | grep -A3 'Group 1' | grep '192.168.1.51' | awk '{print $2}' | sed 's/(//g' | sed 's/)//g')
+  glusterfs_brick3=$(gstatus -ab | grep -A3 'Group 1' | grep '192.168.1.40' | awk '{print $2}' | sed 's/(//g' | sed 's/)//g')
+else
+  glusterfs_brick1="gstatus not found"
+  glusterfs_brick2="gstatus not found"
+  glusterfs_brick3="gstatus not found"
+fi
+
+if [ -x /usr/sbin/gluster ]; then
+  gluster_version=$(gluster --version | head -n1 | awk '{print $2}')
+else
+  gluster_version="GlusterFS not found."
+fi
 
 # set colors for status
 if [[ $glusterfs_brick1 = "Online" ]]; then
@@ -19,10 +29,10 @@ else
   glusterfs_brick2_colored=$COLOR_RED$glusterfs_brick2$COLOR_CLOSE$COLOR_YELLOW
 fi
 
-if [[ $glusterfs_arbiter = "Online" ]]; then
-  glusterfs_arbiter_colored=$COLOR_GREEN$glusterfs_arbiter$COLOR_CLOSE$COLOR_YELLOW
+if [[ $glusterfs_brick3 = "Online" ]]; then
+  glusterfs_brick3_colored=$COLOR_GREEN$glusterfs_brick3$COLOR_CLOSE$COLOR_YELLOW
 else
-  glusterfs_arbiter_colored=$COLOR_RED$glusterfs_arbiter$COLOR_CLOSE$COLOR_YELLOW
+  glusterfs_brick3_colored=$COLOR_RED$glusterfs_brick3$COLOR_CLOSE$COLOR_YELLOW
 fi
 
 # MOTD
@@ -30,7 +40,7 @@ echo -e " "$COLOR_BLUE"Proxmox   "$COLOR_CLOSE`echo -e "$COLOR_GREEN$(pveversion
 echo
 echo -e " "$COLOR_LIGHT_BLUE"GlusterFS ("$gluster_version")"$COLOR_CLOSE
 echo
-echo -e " "$COLOR_YELLOW"Brick-1: "$glusterfs_brick1_colored" Brick-2: "$glusterfs_brick2_colored" Arbiter: "$glusterfs_arbiter_colored$COLOR_CLOSE
+echo -e " "$COLOR_YELLOW"Brick-1: "$glusterfs_brick1_colored" Brick-2: "$glusterfs_brick2_colored" Brick-3: "$glusterfs_brick3_colored$COLOR_CLOSE
 echo
 echo -e " "$COLOR_LIGHT_BLUE"LXC"$COLOR_CLOSE
 echo
