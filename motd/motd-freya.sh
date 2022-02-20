2acc3f5fecb94ab91d428a3c29a15e6e33eaf8e5
Thorsten Walk <darkiop@gmail.com>
2022-02-20 08:53:24 +0100

    motd

--- motd/motd-freya.sh
+++ motd/motd-freya.sh
@@ -1,31 +1,44 @@
 #!/bin/bash
 
-glusterfs_brick1=$(gluster volume heal glusterfs-1-volume info | grep -A 1 '192.168.1.50' | tail -n 1 | awk '{print $2}')
-glusterfs_brick2=$(gluster volume heal glusterfs-1-volume info | grep -A 1 '192.168.1.51' | tail -n 1 | awk '{print $2}')
-glusterfs_arbiter=$(gluster volume heal glusterfs-1-volume info | grep -A 1 '192.168.1.40' | tail -n 1 | awk '{print $2}')
+if [ -x /usr/local/bin/gstatus ]; then
+  glusterfs_brick1=$(gstatus -ab | grep -A3 'Group 1' | grep '192.168.1.50' | awk '{print $2}' | sed 's/(//g' | sed 's/)//g')
+  glusterfs_brick2=$(gstatus -ab | grep -A3 'Group 1' | grep '192.168.1.51' | awk '{print $2}' | sed 's/(//g' | sed 's/)//g')
+  glusterfs_brick3=$(gstatus -ab | grep -A3 'Group 1' | grep '192.168.1.40' | awk '{print $2}' | sed 's/(//g' | sed 's/)//g')
+else
+  glusterfs_brick1="gstatus not found"
+  glusterfs_brick2="gstatus not found"
+  glusterfs_brick3="gstatus not found"
+fi
 
-if [ $glusterfs_brick1 == "Connected" ]; then
+if [ -x /usr/sbin/gluster ]; then
+  gluster_version=$(gluster --version | head -n1 | awk '{print $2}')
+else
+  gluster_version="GlusterFS not found."
+fi
+
+# set colors for status
+if [[ $glusterfs_brick1 = "Online" ]]; then
   glusterfs_brick1_colored=$COLOR_GREEN$glusterfs_brick1$COLOR_CLOSE$COLOR_YELLOW
 else
   glusterfs_brick1_colored=$COLOR_RED$glusterfs_brick1$COLOR_CLOSE$COLOR_YELLOW
 fi
 
-if [ $glusterfs_brick2 == "Connected" ]; then
+if [[ $glusterfs_brick2 = "Online" ]]; then
   glusterfs_brick2_colored=$COLOR_GREEN$glusterfs_brick2$COLOR_CLOSE$COLOR_YELLOW
 else
   glusterfs_brick2_colored=$COLOR_RED$glusterfs_brick2$COLOR_CLOSE$COLOR_YELLOW
 fi
 
-if [ $glusterfs_arbiter == "Connected" ]; then
-  glusterfs_arbiter_colored=$COLOR_GREEN$glusterfs_arbiter$COLOR_CLOSE$COLOR_YELLOW
+if [[ $glusterfs_brick3 = "Online" ]]; then
+  glusterfs_brick3_colored=$COLOR_GREEN$glusterfs_brick3$COLOR_CLOSE$COLOR_YELLOW
 else
-  glusterfs_arbiter_colored=$COLOR_RED$glusterfs_arbiter$COLOR_CLOSE$COLOR_YELLOW
+  glusterfs_brick3_colored=$COLOR_RED$glusterfs_brick3$COLOR_CLOSE$COLOR_YELLOW
 fi
 
 echo
 echo -e " "$COLOR_LIGHT_BLUE"GlusterFS"$COLOR_CLOSE
 echo
-echo -e "" $COLOR_YELLOW"Brick-1: "$glusterfs_brick1_colored" Brick-2: "$glusterfs_brick2_colored" Arbiter: "$glusterfs_arbiter_colored$COLOR_CLOSE
+echo -e "" $COLOR_YELLOW"Brick-1: "$glusterfs_brick1_colored" Brick-2: "$glusterfs_brick2_colored" Brick-3: "$glusterfs_brick3_colored$COLOR_CLOSE
 echo
 
 #EOF
