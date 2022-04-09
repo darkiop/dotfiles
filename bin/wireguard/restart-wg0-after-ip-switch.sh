4535cb1cb83815763591bfdb29dd7f3d37c52771
Thorsten Walk <darkiop@gmail.com>
2022-04-09 07:01:53 +0200

    misc

--- bin/wireguard/restart-wg0-after-ip-switch.sh
+++ bin/wireguard/restart-wg0-after-ip-switch.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 DATE=$(date +%Y-%m-%d_%H:%M:%S)
-LOGFILE="/var/log/wg-check-endpoint.log"
+LOGFILE="/var/log/wg-check-birkenweg.log"
 DYNDNS=""
-
 IP1=$(wg | grep endpoint | awk '{print $2}' | sed 's/:.*//')
-IP2=$(nslookup $DYNDNS | grep -A2 Name | grep Address | awk '{print $2}')
+IP2=$(nslookup $DYNDNS | grep -A2 Name | grep Address | awk '{print $2}' | tail -n1)
 
 if [ "$IP1" != "$IP2" ]; then
   systemctl restart wg-quick@wg0.service
-  echo $DATE" - IP of $DYNDNS != IP wg0-endpoint - restart wg-quick@wg0.service" >> ${LOGFILE}
+  echo $DATE" - IP "$DYNDNS" <> wg0-endpoint - restart wg-quick@wg0.service"
+  echo $DATE" - IP "$DYNDNS" <> wg0-endpoint - restart wg-quick@wg0.service" >> ${LOGFILE}
+else
+  echo $IP1 "=" $IP2 " nothing todo"
 fi
