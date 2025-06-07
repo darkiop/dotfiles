ee0a2419ed3048eb30dae10fc8fab1efa5662f7a
Thorsten Walk <darkiop@gmail.com>
2025-06-07 06:28:12 +0200

    install script optimized

--- install.sh
+++ install.sh
@@ -1,24 +1,19 @@
 #!/usr/bin/env bash
 
-# -------------------------------------------------------------
-# always exit on error
-# -------------------------------------------------------------
+# Always exit immediately if a command exits with a non-zero status
 set -e
 
-# -------------------------------------------------------------
 # check root function
-# https://stackoverflow.com/questions/18215973/how-to-check-if-running-as-root-in-a-bash-script
-# -------------------------------------------------------------
-function is_user_root() {
-	[[ "$(id -u)" -eq 0 ]]
+function IS_USER_ROOT() {
+	local uid
+	uid=$(id -u) || return 1
+	[[ ${uid} -eq 0 ]]
 }
 
-# -------------------------------------------------------------
 # check if user is root and if not exit
-# -------------------------------------------------------------
 function if_user_root_msg() {
-	if [[ is_user_root ]]; then
-		message red "You need to run this as root. Exit."
+	if IS_USER_ROOT; then
+		MESSAGE red "You need to run this as root. Exit."
 		exit 1
 	fi
 }
@@ -26,87 +21,94 @@ function if_user_root_msg() {
 # -------------------------------------------------------------
 # first check if root, when not define a alias with sudo
 # -------------------------------------------------------------
-if [[ is_user_root ]]; then
-	apt=$(whereis apt)
+if IS_USER_ROOT; then
+	APT=$(whereis apt)
 else
-	apt='sudo '$(whereis apt)
+	APT='sudo '$(whereis apt)
 fi
 
 # -------------------------------------------------------------
-# load color vars
-# https://bashcolors.com
+# load COLOR vars
+# https://bashCOLORs.com
 # -------------------------------------------------------------
-function loadColors() {
+function LOAD_COLORS() {
 	if [[ ! -f "${HOME}/dotfiles/config/dotfiles.config" ]]; then
-		source <(curl -s https://raw.githubusercontent.com/darkiop/dotfiles/master/config/dotfiles.config)
+		# shellcheck source=/dev/null
+		config_url='https://raw.githubusercontent.com/darkiop/dotfiles/master/config/dotfiles.config'
+		# trunk-ignore(shellcheck/SC1090)
+		# trunk-ignore(shellcheck/SC2312)
+		source <(curl -s "${config_url}")
 	else
-		source "${HOME}/dotfiles/config/dotfiles.config"
+		# shellcheck source=/dev/null
+		source ~/dotfiles/config/dotfiles.config
 	fi
 }
 
 # -------------------------------------------------------------
-# check if sudo is installed
+# check if sudo is installedq
 # -------------------------------------------------------------
-function check_if_sudo_is_installed() {
-	#if [[ ! -n "$(whereis sudo)" ]]; then
-  if [[ -z "$(whereis sudo || true )" ]]; then
-		message red "sudo not found. install it ..."
-		${apt} install sudo -y
+function CHECK_IF_SUDO_IS_INSTALLED() {
+	# trunk-ignore(shellcheck/SC2312)
+	if [[ -z "$(whereis sudo)" ]]; then
+		MESSAGE red "sudo not found. install it ..."
+		${APT} install sudo -y
 	fi
 }
 
 # -------------------------------------------------------------
 # check if curl is installed
 # -------------------------------------------------------------
-function check_if_curl_is_installed() {
-	if [[ ! "$(whereis curl)" ]]; then
-		message red "curl not found. install it ..."
-		${apt} install curl -y
+function CHECK_IF_CURL_IS_INSTALLED() {
+	# trunk-ignore(shellcheck/SC2312)
+	if [[ -z "$(whereis curl)" ]]; then
+		MESSAGE red "curl not found. install it ..."
+		${APT} install curl -y
 	fi
 }
 
 # -------------------------------------------------------------
 # check if git is installed
 # -------------------------------------------------------------
-function check_if_git_is_installed() {
-	if [[ ! "$(whereis git)" ]]; then
-		message red "git not found. install it ..."
-		${apt} install git -y
+function CHECK_IF_GIT_IS_INSTALLED() {
+	# trunk-ignore(shellcheck/SC2312)
+	if [[ -z "$(whereis git)" ]]; then
+		MESSAGE red "git not found. install it ..."
+		${APT} install git -y
 	fi
 }
 
 # -------------------------------------------------------------
-# Ask
+# ASK
 # example:
-#   ask blue "Question?"
+#   ASK blue "Question?"
 #   if [ $REPLY == "y" ]; then
 #     do something ...
 #   fi
 # -------------------------------------------------------------
-function ask() {
-	local color="$1"
-	case ${color} in
+function ASK() {
+	local COLOR="$1"
+	case ${COLOR} in
 	green)
-		color=${COLOR_GREEN}
+		COLOR=${COLOR_GREEN}
 		;;
 	blue)
-		color=${COLOR_BLUE}
+		COLOR=${COLOR_BLUE}
 		;;
 	lightblue)
-		color=${COLOR_LIGHT_BLUE}
+		COLOR=${COLOR_LIGHT_BLUE}
 		;;
 	yellow)
-		color=${COLOR_YELLOW}
+		COLOR=${COLOR_YELLOW}
 		;;
 	red)
-		color=${COLOR_RED}
+		COLOR=${COLOR_RED}
 		;;
 	*)
-		color=${COLOR_DEFAULT}
+		COLOR=${COLOR_DEFAULT}
 		;;
 	esac
 	while true; do
-		echo -e "${color}"
+		echo -e "${COLOR}"
 		read -p "$2 ([y]/n) " -r
 		echo -e "${COLOR_CLOSE}"
 		REPLY=${REPLY:-"y"}
@@ -119,32 +121,32 @@ function ask() {
 }
 
 # -------------------------------------------------------------
-# Message
-# example: message color "text"
+# MESSAGE
+# example: MESSAGE COLOR "text"
 # -------------------------------------------------------------
-function message() {
-	local color="$1"
-	case ${color} in
+function MESSAGE() {
+	local COLOR="$1"
+	case ${COLOR} in
 	green)
-		color=${COLOR_GREEN}
+		COLOR=${COLOR_GREEN}
 		;;
 	blue)
-		color=${COLOR_BLUE}
+		COLOR=${COLOR_BLUE}
 		;;
 	lightblue)
-		color=${COLOR_LIGHT_BLUE}
+		COLOR=${COLOR_LIGHT_BLUE}
 		;;
 	yellow)
-		color=${COLOR_YELLOW}
+		COLOR=${COLOR_YELLOW}
 		;;
 	red)
-		color=${COLOR_RED}
+		COLOR=${COLOR_RED}
 		;;
 	*)
-		color=${COLOR_DEFAULT}
+		COLOR=${COLOR_DEFAULT}
 		;;
 	esac
-	echo -e "${color}"
+	echo -e "${COLOR}"
 	echo "$2"
 	echo -e "${COLOR_CLOSE}"
 }
@@ -152,14 +154,14 @@ function message() {
 # -------------------------------------------------------------
 # main menu
 # -------------------------------------------------------------
-function show_main_menu() {
+function SHOW_MAIN_MENU() {
 	unset opt_main_menu
 	echo
 	# Use printf '%b' to expand backslash escapes in the variable:
 	printf '%b[ darkiop/dotfiles ]%b\n\n' \
 		"${COLOR_GREEN}" "${COLOR_CLOSE}"
 
-	# Now use %b for colors, then literal “1)”, then %b to close color:
+	# Now use %b for COLORs, then literal “1)”, then %b to close COLOR:
 	printf '%b1)%b Install dotfiles (all)\n' \
 		"${COLOR_YELLOW}" "${COLOR_CLOSE}"
 
@@ -181,14 +183,14 @@ function show_main_menu() {
 # -------------------------------------------------------------
 # clone repo from github
 # -------------------------------------------------------------
-function cloneREPO() {
+function CLONE_REPO() {
 	if [[ ! -d "${HOME}/dotfiles" ]]; then
-		message blue "[ clone dotfiles repo from github ]"
+		MESSAGE blue "[ clone dotfiles repo from github ]"
 		git clone --recurse-submodules https://github.com/darkiop/dotfiles "${HOME}/dotfiles"
 		cd "${HOME}/dotfiles"
 		git config pull.rebase false
 	else
-		message yellow "dotfiles directory already exist."
+		MESSAGE yellow "dotfiles directory already exist."
 	fi
 }
 
@@ -196,31 +198,31 @@ function cloneREPO() {
 # Install bash_completition.d
 # -------------------------------------------------------------
 #function instBASHCOMPLE() {
-#  message blue "[ Install bash_completitions ]"
-#  if [ -L "$HOME/.bash_completion.d" ] ; then
-#    if [ ! -e "$HOME/.bash_completion.d" ] ; then
+#  MESSAGE blue "[ Install bash_completitions ]"
+#  if [ -L "~/.bash_completion.d" ] ; then
+#    if [ ! -e "~/.bash_completion.d" ] ; then
 #        # remove > broken
-#        rm "$HOME/.bash_completion.d"
+#        rm "~/.bash_completion.d"
 #        echo -e "$COLOR_GREEN""create""$COLOR_CLOSE""$COLOR_YELLOW"" bash_completion.d ""$COLOR_GREEN""symlink ...""$COLOR_CLOSE"
-#        ln -s "$HOME"/dotfiles/bash_completion.d ~/.bash_completion.d
+#        ln -s "~"/dotfiles/bash_completion.d ~/.bash_completion.d
 #    fi
 #  else
 #    # link not exist
 #    echo -e "$COLOR_GREEN""create""$COLOR_CLOSE""$COLOR_YELLOW"" bash_completion.d ""$COLOR_GREEN""symlink ...""$COLOR_CLOSE"
-#    ln -s "$HOME"/dotfiles/bash_completion.d ~/.bash_completion.d
+#    ln -s "~"/dotfiles/bash_completion.d ~/.bash_completion.d
 #  fi
 #
 #  # argcomplete
 #  # https://github.com/kislyuk/argcomplete
-#  if [ ! is_user_root ]; then
+#  if [ ! IS_USER_ROOT ]; then
 #    if [[ ! -x /usr/local/bin/activate-global-python-argcomplete ]]; then
 #      pip3 install argcomplete
-#      if [ -f "$HOME"/.local/bin/activate-global-python-argcomplete ]; then
-#        "$HOME"/.local/bin/activate-global-python-argcomplete --dest="$HOME"/dotfiles/bash_completion.d
+#      if [ -f "~"/.local/bin/activate-global-python-argcomplete ]; then
+#        "~"/.local/bin/activate-global-python-argcomplete --dest="~"/dotfiles/bash_completion.d
 #      fi
 #    else
-#      if [ -f "$HOME"/.local/bin/activate-global-python-argcomplete ]; then
-#        "$HOME"/.local/bin/activate-global-python-argcomplete --dest="$HOME"/dotfiles/bash_completion.d
+#      if [ -f "~"/.local/bin/activate-global-python-argcomplete ]; then
+#        "~"/.local/bin/activate-global-python-argcomplete --dest="~"/dotfiles/bash_completion.d
 #      fi
 #    fi
 #  fi
@@ -229,14 +231,14 @@ function cloneREPO() {
 # -------------------------------------------------------------
 # install: vimrc-amix
 # -------------------------------------------------------------
-function instVIMRC() {
+function INSTALL_VIMRC() {
 	VIMRC_INSTALL="${HOME}/dotfiles/modules/vimrc/install_awesome_parameterized.sh"
-	message blue "[ Install vimrc ]"
-  message lightblue "Creating symlink for vimrc runtime directory from ${HOME}/dotfiles/modules/vimrc to ${HOME}/.vim_runtime"
-  ln -sf -- "${HOME}"/dotfiles/modules/vimrc "${HOME}/.vim_runtime"
-  message lightblue "Creating symlink for vimrc configuration file from ${HOME}/dotfiles/config/vimrc/my_configs.vim to ${HOME}/.vim_runtime/my_configs.vim"
-  ln -sf -- "${HOME}"/dotfiles/config/vimrc/my_configs.vim "${HOME}/.vim_runtime/my_configs.vim"
-  echo
+	MESSAGE blue "[ Install vimrc ]"
+	MESSAGE lightblue "Creating symlink for vimrc runtime directory from ${HOME}/dotfiles/modules/vimrc to ${HOME}/.vim_runtime"
+	ln -sf -- "${HOME}"/dotfiles/modules/vimrc "${HOME}/.vim_runtime"
+	MESSAGE lightblue "Creating symlink for vimrc configuration file from ${HOME}/dotfiles/config/vimrc/my_configs.vim to ${HOME}/.vim_runtime/my_configs.vim"
+	ln -sf -- "${HOME}"/dotfiles/config/vimrc/my_configs.vim "${HOME}/.vim_runtime/my_configs.vim"
+	echo
 	bash "${VIMRC_INSTALL}" "${HOME}"/dotfiles/modules/vimrc "${USER}"
 	echo
 }
@@ -244,27 +246,24 @@ function instVIMRC() {
 # -------------------------------------------------------------
 # install: oh-my-tmux
 # -------------------------------------------------------------
-function instTMUX() {
-	message blue "[ Install oh-my-tmux and tmux plugin manager ]"
+function INSTALL_TMUX() {
+	MESSAGE blue "[ Install oh-my-tmux and tmux plugin manager ]"
 	if [[ ! -d "${HOME}"/.tmux/plugins ]]; then
 		mkdir -p "${HOME}"/.tmux/plugins
 	fi
-  message lightblue "Creating symlink for .tmux.conf from dotfiles/modules/oh-my-tmux/.tmux.conf to ~/.tmux.conf"
-  ln -sf -- "${HOME}"/dotfiles/modules/oh-my-tmux/.tmux.conf "${HOME}"/.tmux.conf
-
-  message lightblue "Creating symlink for tmux plugin manager (tpm) from dotfiles/modules/tpm to ~/.tmux/plugins/tpm"
-  ln -sf -- "${HOME}"/dotfiles/modules/tpm "${HOME}"/.tmux/plugins/tpm
-
-  message lightblue "Creating symlink for .tmux.conf.local from dotfiles/config/tmux.conf.local to ~/.tmux.conf.local"
-  ln -sf -- "${HOME}"/dotfiles/config/tmux.conf.local "${HOME}"/.tmux.conf.local
-	echo
+	MESSAGE lightblue "Creating symlink for .tmux.conf from dotfiles/modules/oh-my-tmux/.tmux.conf to ~/.tmux.conf"
+	ln -sf -- "${HOME}"/dotfiles/modules/oh-my-tmux/.tmux.conf "${HOME}"/.tmux.conf
+	MESSAGE lightblue "Creating symlink for tmux plugin manager (tpm) from dotfiles/modules/tpm to ~/.tmux/plugins/tpm"
+	ln -sf -- "${HOME}"/dotfiles/modules/tpm "${HOME}"/.tmux/plugins/tpm
+	MESSAGE lightblue "Creating symlink for .tmux.conf.local from dotfiles/config/tmux.conf.local to ~/.tmux.conf.local"
+	ln -sf -- "${HOME}"/dotfiles/config/tmux.conf.local "${HOME}"/.tmux.conf.local
 }
 
 # -------------------------------------------------------------
 # Install .bashrc
 # -------------------------------------------------------------
-function instBASHRC() {
-	message blue "[ Install .bashrc ]"
+function LINK_DOTFILES() {
+	MESSAGE blue "[ Install .bashrc ]"
 
 	# install
 	dir="${HOME}"/dotfiles
@@ -274,24 +273,24 @@ function instBASHRC() {
 	echo -e "${COLOR_GREEN}""Delete""${COLOR_CLOSE}""${COLOR_YELLOW}"" old ""${COLOR_GREEN}""symlinks ...""${COLOR_CLOSE}"
 	for file in ${files}; do
 		if [[ -f "${HOME}"/."${file}" ]]; then
-      echo "Deleting existing symlink for configuration file ~/.${file} to prepare for a fresh installation."
+			echo "Deleting existing symlink for configuration file ~/.${file} to prepare for a fresh installation."
 			rm "${HOME}"/."${file}"
 		fi
 	done
 	echo
 	# new symlinks for files
 	echo -e "${COLOR_GREEN}""Create""${COLOR_CLOSE}""${COLOR_YELLOW}"" new ""${COLOR_GREEN}""symlinks ...""${COLOR_CLOSE}"
-  for file in ${files}; do
-    echo "Creating symlink for ${dir}/${file} to ~/.${file}"
-    ln -s "${dir}/${file}" ~/."${file}"
-  done
+	for file in ${files}; do
+		echo "Creating symlink for ${dir}/${file} to ~/.${file}"
+		ln -s "${dir}/${file}" ~/."${file}"
+	done
 	echo
 }
 
 # -------------------------------------------------------------
 # load .bashrc
 # -------------------------------------------------------------
-function loadBASHRC() {
+function LOAD_BASHRC() {
 	echo
 	echo -e "${COLOR_YELLOW}""[ dotfiles installed ]""${COLOR_CLOSE}"
 	echo -e "${COLOR_RED}"
@@ -302,7 +301,7 @@ function loadBASHRC() {
 		su - "${USER}"
 		;;
 	n | N | *)
-		message yellow "Do nothing and exit."
+		MESSAGE yellow "Do nothing and exit."
 		exit
 		;;
 	esac
@@ -311,17 +310,17 @@ function loadBASHRC() {
 # -------------------------------------------------------------
 # install: dotfiles (all)
 # -------------------------------------------------------------
-function instDOTF() {
-	message yellow "+++ Install dotfiles (all) +++"
-	check_if_sudo_is_installed
-	check_if_curl_is_installed
-	loadColors
-	check_if_git_is_installed
-	cloneREPO
+function INSTALL_DOTFILES() {
+	MESSAGE yellow "+++ Install dotfiles (all) +++"
+	CHECK_IF_SUDO_IS_INSTALLED
+	CHECK_IF_CURL_IS_INSTALLED
+	LOAD_COLORS
+	CHECK_IF_GIT_IS_INSTALLED
+	CLONE_REPO
 	#instBASHCOMPLE
-	instVIMRC
-	instTMUX
-	instBASHRC
+	INSTALL_VIMRC
+	INSTALL_TMUX
+	LINK_DOTFILES
 }
 
 # -------------------------------------------------------------
@@ -329,34 +328,34 @@ function instDOTF() {
 # -------------------------------------------------------------
 if [[ $1 == 'all' ]]; then
 	# skip menu and install all
-	instDOTF
+	INSTALL_DOTFILES
 	if [[ $2 == 'load-bashrc' ]]; then
-		loadBASHRC
+		LOAD_BASHRC
 	fi
 else
-	show_main_menu
+	SHOW_MAIN_MENU
 	if [[ ${opt_main_menu} == '' ]]; then
 		exit
 	else
 		case ${opt_main_menu} in
 		1) # install dotfiles
-			instDOTF
+			INSTALL_DOTFILES
 			exit
 			;;
 		2) # install .bashrc
-			instBASHRC
+			LINK_DOTFILES
 			;;
 		x | X) # exit
 			exit
 			;;
 		* | \n) # typo - show main menu again
-			show_main_menu
+			SHOW_MAIN_MENU
 			;;
 		x | X) # exit
 			exit
 			;;
 		* | \n) # typo - show main menu again
-			show_main_menu
+			SHOW_MAIN_MENU
 			;;
 		esac
 	fi
