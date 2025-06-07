39628260a54e036eb16a8e76c0dcf239a08db087
Thorsten Walk <darkiop@gmail.com>
2025-06-07 06:28:46 +0200

    autoupdate script

--- autoupdate.sh
+++ autoupdate.sh
@@ -4,19 +4,17 @@
 COUNT=~/.dotfiles-update-count
 STARTUP_COUNT=
 
-if [[ -f "${COUNT}" ]]; then
-  STARTUP_COUNT=$(cat "${COUNT}")
-  # trunk-ignore(shellcheck/SC2004)
-  echo $((${STARTUP_COUNT} + 1)) > "${COUNT}"
-  if [[ ${STARTUP_COUNT} -gt 20 ]]
-  then
-      echo "Update dotfiles ..."
-      cd ~/dotfiles || exit
-      git pull
-      echo "0" > "${COUNT}"
-  fi
-else 
-  echo "0" > "${COUNT}"
+if [[ -f ${COUNT} ]]; then
+	STARTUP_COUNT=$(cat "${COUNT}")
+	echo $((STARTUP_COUNT + 1)) >"${COUNT}"
+	if [[ ${STARTUP_COUNT} -gt 20 ]]; then
+		echo "Update dotfiles ..."
+		cd ~/dotfiles || exit
+		git pull
+		echo "0" >"${COUNT}"
+	fi
+else
+	echo "0" >"${COUNT}"
 fi
 
 # EOF
