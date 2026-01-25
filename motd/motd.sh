0851cbc1e8eba07562dc9920523173fd4eeccf54
Thorsten Walk <darkiop@gmail.com>
2026-01-25 16:28:09 +0100

    Remove dots from MOTD IPs for testing

--- motd/motd.sh
+++ motd/motd.sh
@@ -180,11 +180,9 @@ fi
 # BUILD THE MOTD OUTPUT
 _motd_defang_ips() {
 	local value="$1"
-	local dot
 
-	# Use a non-ASCII dot to avoid terminal auto-linking of IPs.
-	dot=$'\u2024'
-	value=${value//./${dot}}
+	# Remove dots to avoid any terminal auto-linking of IPs.
+	value=${value//./}
 	printf "%s" "${value}"
 }
 
